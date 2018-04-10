f2d935fd9e8d43993c43c2cf3c4a69910f0d4c1c
b2blosangeles <b2blosangeles@gmail.com>
2018-04-10 07:41:54 -0700

    Update cron_watch.js

--- api/cron_watch.js
+++ api/cron_watch.js
@@ -20,7 +20,7 @@ pkg.fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
                 res.send('normal');
             }
         } else {
-            res.send('watch error!!');
+            res.send('watch error-!!');
         }
   }
 });	 
