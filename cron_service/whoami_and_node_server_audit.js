2a87884e550ebcc31b665ad4df4a8ac8d69d3713
b2blosangeles <b2blosangeles@gmail.com>
2018-04-10 14:47:29 -0700

    Update whoami_and_node_server_audit.js

--- cron_service/whoami_and_node_server_audit.js
+++ cron_service/whoami_and_node_server_audit.js
@@ -1,29 +1,8 @@
 /* --- code for cron watch ---*/
-(function(){
-	console.log('watch ===>' + new Date());
-    var path = require('path');
-    var env = {root_path:path.join(__dirname, '../../..')};
-    env.site_path = env.root_path + '/sites/root';
-    var request =  require(env.root_path + '/package/request/node_modules/request');
-    var fs = require('fs');
-
-    var watch0 = {start:new Date(), mark:new Date()};
-    fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
-      if (err){
-	  fs.writeFile('/var/.qalet_cron_watch.data', JSON.stringify(watch0), function (err) {});
-      } else {
-	var watch = {};
-	try { watch = JSON.parse(data);} catch (e) {}
-	if (watch.mark)  {
-	  delete watch.start;
-	  watch.mark = new Date();
-	  fs.writeFile('/var/.qalet_cron_watch.data', JSON.stringify(watch), function (err) {
-	      console.log(watch);
-	  });
-	} 
-      }
-    });	 
-})();
+delete require.cache[__dirname + '/watch_cron.inc.js'];
+let watch_cron_inc = require(__dirname + '/watch_cron.inc.js'),
+    watchCron = new watch_cron_inc(__filename);
+watchCron.load('root', 60);
 
 /* --- code for audit ---*/
 let path = require('path'), 
