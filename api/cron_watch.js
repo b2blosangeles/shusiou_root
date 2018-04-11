403f787ab2cdcabfe1deedbd3f57ba34309d3f5d
b2blosangeles <b2blosangeles@gmail.com>
2018-04-10 17:20:25 -0700

    Update cron_watch.js

--- api/cron_watch.js
+++ api/cron_watch.js
@@ -1,25 +1,4 @@
 /* if cron stopped root server will be reboot */
-/* if cron stopped root server will be reboot */
-pkg.fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
-  if (err){
-      res.send(err.message);
-  } else {
-        var watch = {};
-        try { watch = JSON.parse(data);} catch (e) {}
-        var result_a = [];
-        for (var o in watch) {
-            let t1 = new Data(watch[o].start).getTime();
-            let t2 = new Data(watch[o].mark).getTime();
-            let scheduled = watch[o].scheduled;
-            if ((t1) && (scheduled) && (t2 - t1 > scheduled * 3) {
-                result_a.push(o);
-            }
-        }
-        res.send({status:'success', result_a:result_a, data:data});
-  }
-});	
-/*
-
 pkg.fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
   if (err){
       res.send(err.message);
@@ -41,8 +20,7 @@ pkg.fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
                 res.send('normal');
             }
         } else {
-            res.send('watch error!!');
+            res.send({status:'error', data:data});
         }
   }
-});	 
-*/
+});
