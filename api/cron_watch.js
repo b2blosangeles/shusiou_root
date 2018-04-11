8ea55328057e6dee5996450438d0c129a94eccb8
b2blosangeles <b2blosangeles@gmail.com>
2018-04-10 17:30:47 -0700

    Update cron_watch.js

--- api/cron_watch.js
+++ api/cron_watch.js
@@ -7,8 +7,8 @@ pkg.fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
         try { watch = JSON.parse(data);} catch (e) {}
         var result_a = [];
         for (var o in watch) {
-            let t1 = new Data(watch[o].start).getTime();
-            let t2 = new Data(watch[o].mark).getTime();
+            let t1 = (watch[o].start) ? new Data(watch[o].start).getTime() : null;
+            let t2 = (watch[o].mark) ? new Data(watch[o].mark).getTime() : null;
             let scheduled = watch[o].scheduled;
             if ((t1) && (scheduled) && (t2 - t1 > scheduled * 3)) {
                 result_a.push(o);
