d2a75707f8111f3e25ebb43aa52b5034aec01384
b2blosangeles <b2blosangeles@gmail.com>
2018-05-30 17:53:27 -0700

    Update whoami_and_node_server_audit.js

--- cron_service/whoami_and_node_server_audit.js
+++ cron_service/whoami_and_node_server_audit.js
@@ -18,7 +18,7 @@ let mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 
 delete require.cache[env.root_path + '/sites/master/api/inc/socketNodeClient/socketNodeClient.js'];
 var socketNodeClient = require(env.root_path + '/sites/master/api/inc/socketNodeClient/socketNodeClient.js');
-var socketClient = new socketNodeClient('https://' + config.root + '/');
+var socketClient = new socketNodeClient('https://dev.shusiou.win/');
 
 socketClient.sendToRoom(
     'VID_NIU',
