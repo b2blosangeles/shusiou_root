56aeda7cfd36602c6f8ff3344fc7ef3d486640d8
b2blosangeles <b2blosangeles@gmail.com>
2018-05-31 17:09:08 -0700

    Update cron_watch.js

--- api/cron_watch.js
+++ api/cron_watch.js
@@ -15,13 +15,17 @@ pkg.fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
             }
         }
         if (result_a.length) {
+              
               pkg.fs.unlink('/var/.qalet_cron_watch.data',function(err){
                      pkg.fs.appendFile('/var/log/cron_watch.js.reboot.log', "\n\n"+new Date() + ">>\n" + JSON.stringify(result_a), function (err) {
-                        pkg.exec('shutdown -r +0', function(error, stdout, stderr) {
+                       /* 
+                       pkg.exec('shutdown -r +0', function(error, stdout, stderr) {
                           res.send('Server will be reboot in 1 minute!');
                         });
+                        */
                      });                
-               });    
+               }); 
+               
         } else {
             res.send({status:'success', result_a:result_a, data:watch});
         }
