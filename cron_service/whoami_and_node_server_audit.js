add162119353d004a9d0b7d5bc792b5ffba237a1
b2blosangeles <b2blosangeles@gmail.com>
2018-06-22 08:40:54 -0700

    Update whoami_and_node_server_audit.js

--- cron_service/whoami_and_node_server_audit.js
+++ cron_service/whoami_and_node_server_audit.js
@@ -16,15 +16,13 @@ let mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 /* --- code for audit ---*/
 
 /* ------ test code -------*/
-delete require.cache[env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js'];
-var socketNodeClient = require(env.site_path + '/api/inc/socketNodeClient/socketNodeClient.js');
-var socketClient = new socketNodeClient(
-	{link:'https://comm1.service.dev.shusiou.win/'}, 
-	env);
+delete require.cache[env.root_path + '/sites/master/api/inc/socketNodeClient/socketNodeClient.js'];
+var socketNodeClient = require(env.root_path + '/sites/master/api/inc/socketNodeClient/socketNodeClient.js');
 
 var socketClient = new socketNodeClient(
 	{link:'https://comm1.service.dev.shusiou.win/'}, 
 	env);
+
 
 socketClient.sendToRoom(
     'CRON_REPORT',
