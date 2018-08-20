8d83d3596eb1bd544c9ed716ec803a21de87479d
b2blosangeles <b2blosangeles@gmail.com>
2018-08-19 22:19:46 -0700

    Update ddns.js

--- api/inc/ddns/ddns.js
+++ api/inc/ddns/ddns.js
@@ -8,7 +8,63 @@
 		this.validateIPaddress = function (ip)  {
 			let patt = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 			return (patt.test(ip)) ?  true : false;
-  		}		
+  		}
+		this.sendTypeNamedIP = function(type, name, key, db_env, req, res) {
+			let me = this, k, tp = (type === 'master') ? 'm' : (type === 'node') ? 'n' : (type === 'comm') ? 'c' : '';
+	
+			if (['dev', 'qa', 'prod'].indexOf(db_env) === -1 || !tp) {
+				res.end(); 
+				return true;
+			}
+			 if (isNaN(key) || key === '0') { 
+			 	res.end(); 
+				return true;
+			} else {
+				k = parseInt(key) - 1;
+			}
+		
+			if (!_dns[tp].list.length || (new Date().getTime() - _dns[tp].tm) > 60000 ) {
+				var mysql = require(env.sites_path + '/root/api/inc/mysql/node_modules/mysql'),
+				config = require(env.config_path + '/config.json'),
+				cfg0 = config.db;
+				cfg0.database = (db_env === 'prod') ? 'shusiou' : ('shusiou_' +  db_env);
+				let ips = [];
+				var str = 'SELECT `ip` from `cloud_' + type + '` WHERE `score` < 900 ORDER BY `ip` ASC ';
+				var connection = mysql.createConnection(cfg0);
+				connection.connect();
+				connection.query(str, function (error, results, fields) {
+					connection.end();
+					if (error) {
+						return true;
+					} else {
+						if (results) {
+							for (var i = 0; i < results.length; i++) {
+								ips[ips.length] =  results[i].ip;
+							}
+						} else {
+						}
+
+					}
+					_dns[tp].list = ips;
+					_dns[tp].tm = new Date().getTime();
+					me.send([{ 
+						name: name,
+						type: 'A',
+						class: 'IN',
+						ttl: me.ttl,
+						data: ips[k]
+					}], req, res);
+				});
+			} else {
+				me.send([{ 
+					name: name,
+					type: 'A',
+					class: 'IN',
+					ttl: me.ttl,
+					data: _dns[tp].list[k]
+				}], req, res);			
+			}
+		};		
 		this.sendNodeNamedIP = function(name, key, db_env, req, res) {
 			let me = this, k;
 			if (['dev', 'qa', 'prod'].indexOf(db_env) === -1) {
@@ -217,9 +273,7 @@
 			let me = this, question = req.question[0], 
 			    patt = {
 				    ip: /^IP\_([0-9\_]+)\.([a-z0-9]+)\.([a-z0-9]+)$/ig,
-				    node:/^node([0-9]+)\_(qa|dev|prod)\.([a-z0-9]+)\.([a-z0-9]+)$/ig,
-				    comm:/^comm([0-9]+)\_(qa|dev|prod)\.([a-z0-9]+)\.([a-z0-9]+)$/ig,
-				    master:/^master([0-9]+)\_(qa|dev|prod)\.([a-z0-9]+)\.([a-z0-9]+)$/ig,
+				    type:/^(node|master|comm)([0-9]+)\_(qa|dev|prod)\.([a-z0-9]+)\.([a-z0-9]+)$/ig,
 				    db:/^db\.([a-z0-9]+)\.([a-z0-9]+)$/ig,
 				    dns:/^(dev[0-9]+\.|dev\.|qa[0-9]+\.|qa\.|www\.|)([a-z0-9]+)\.([a-z0-9]+)$/ig
 			    },	    
@@ -245,11 +299,12 @@
 						data: ip
 					}], req, res);
 					break;
-				case 'node':
+				case 'type':
 					m = new RegExp(patt[mh]).exec(question.name);
 					console.log(m);
-					me.sendNodeNamedIP(question.name, m[1], m[2], req, res);
+					me.sendNodeNamedIP(m[1], question.name, m[2], m[3], req, res);
 					break;
+					/*
 				case 'comm':
 					m = new RegExp(patt[mh]).exec(question.name);
 					console.log(m);
@@ -258,7 +313,7 @@
 				case 'master':	
 					m = new RegExp(patt[mh]).exec(question.name);
 					me.sendMasterNamedIP(question.name, m[1], m[2], req, res);
-					break;
+					break;*/
 				case 'dns': 
 					me.send([{ 
 						name: question.name,
