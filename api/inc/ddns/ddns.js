43f8e9bfd5b46795c7bcf792e73f2e6118aaaefe
b2blosangeles <b2blosangeles@gmail.com>
2018-08-22 09:23:55 -0700

    Update ddns.js

--- api/inc/ddns/ddns.js
+++ api/inc/ddns/ddns.js
@@ -140,7 +140,7 @@
 						type: 'A',
 						class: 'IN',
 						ttl: me.ttl,
-						data: (_dns.dns.DNS[question.name]) ? _dns.dns.DNS[question.name] : '111.111.111.111'
+						data: (_dns.dns.DNS[question.name]) ? _dns.dns.DNS[question.name] : null
 					}], req, res);				
 					break;	
 				case 'db': 
