11af62bea76d5cf7fccb3d60397c5286442f0099
b2blosangeles <b2blosangeles@gmail.com>
2018-08-21 22:20:51 -0700

    Update IframPlugin.js

--- api/IframPlugin.js
+++ api/IframPlugin.js
@@ -1,5 +1,7 @@
 let code = req.query['code'];
 if (code === 'SR') {
+    res.send(env.site_path + '/tpl/SR.html');
+    return true;
     pkg.fs.readFile(env.site_path + '/tpl/SR.html', 'utf-8', function(err, content) {
           res.send(content.replace(/\{\$room\}/ig, room).replace(/\{\$socket\}/ig, socket));
     });	
