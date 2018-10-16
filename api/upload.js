19d7483a03f7fb165d77e5a3aa88b7019a57d25d
b2blosangeles <b2blosangeles@gmail.com>
2018-10-16 13:41:45 -0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,2 +1,5 @@
-var buf = new Buffer(req.body.data, 'base64');
-res.send({buf:buf});
+var base64Data = req.data.replace(/^data:image\/png;base64,/, "");
+
+require("fs").writeFile("/tmp/out.png", base64Data, 'base64', function(err) {
+    res.send({buf:'buff'});
+});
