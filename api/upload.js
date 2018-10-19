0c2d8b07baef732cc05bee8fb4ce0d31978e26f6
b2blosangeles <b2blosangeles@gmail.com>
2018-10-19 14:15:03 -0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,9 @@
 var pos = req.body.pos,
     base64Data = req.body.data.replace(/^data:image\/png;base64,/, "");
 
-require("fs").writeFile("/tmp/" + pos + "_out.png", base64Data, 'base64', function(err) {
+var folderP = require(env.site_path + '/api/inc/folderP/folderP');
+var fp = new folderP();
+fp.build("/tmp/niu", () => {
+    require("fs").writeFile("sec_" + pos + "_out.png", base64Data, 'base64', function(err) {
     res.send({buf:'buff'});
 });
