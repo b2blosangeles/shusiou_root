e6d9854848f8bf099282fe9586b9504190e41cfb
b2blosangeles <b2blosangeles@gmail.com>
2018-10-19 21:17:23 -0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -3,16 +3,18 @@ var pos = req.body.pos, ses = (!req.body.ses) ? (new Date().getTime()) : req.bod
 
 var folderP = require(env.site_path + '/api/inc/folderP/folderP');
 var fp = new folderP();
-var d_folder = '/tmp/niu_' + ses;
+var d_folder = '/tmp/niu_' + ses, 
+    f_target = '/tmp/pp.png';
+
 fp.build(d_folder, () => {
     if (pos !== 'finished') {
         pkg.fs.writeFile(d_folder + '/sec_' + pos + '_out.png', base64Data, 'base64', function(err) {
             res.send({ses:ses});
         });
     } else {
-        pkg.exec('cd ' + d_folder + '&& cat $(find ./ -name \"sec_*_out.png\" | sort -V) > ../pp1.png && cd /tmp && rm -fr ' + d_folder, 
+        pkg.exec('cat $(find ' + d_folde + '/ -name \"sec_*_out.png\" | sort -V) > ' + f_target + ' && rm -fr ' + d_folder, 
             function (error, stdout, stderr) {
-                res.send({status:'success', message:'finished66'});	
+                res.send({status:'success', message:'finished88'});	
             });
             
     }
