07a2bcdcf5c8f1e0ecc5b797db112a24f5691dbd
b2blosangeles <b2blosangeles@gmail.com>
2018-10-19 21:20:17 -0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -3,7 +3,7 @@ var pos = req.body.pos, ses = (!req.body.ses) ? (new Date().getTime()) : req.bod
 
 var folderP = require(env.site_path + '/api/inc/folderP/folderP');
 var fp = new folderP();
-var d_folder = '/tmp/niu_' + ses, 
+var d_folder = '/tmp/niu_' + ses + '/', 
     f_target = '/tmp/pp.png';
 
 fp.build(d_folder, () => {
@@ -12,7 +12,7 @@ fp.build(d_folder, () => {
             res.send({ses:ses});
         });
     } else {
-        pkg.exec('cat $(find ' + d_folde + '/ -name \"sec_*_out.png\" | sort -V) > ' + f_target + ' && rm -fr ' + d_folder, 
+        pkg.exec('cat $(find ' + d_folde + ' -name "sec_*_out.png" | sort -V) > ' + f_target + ' && rm -fr ' + d_folder, 
             function (error, stdout, stderr) {
                 res.send({status:'success', message:'finished88'});	
             });
