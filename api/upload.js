184bdfab3cedeb06d39d64a20659a3d15a241737
b2blosangeles <b2blosangeles@gmail.com>
2018-10-22 13:39:33 -0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -9,12 +9,12 @@ var d_folder = '/tmp/niu_' + ses + '/',
 fp.build(d_folder, () => {
     if (pos !== 'finished') {
         pkg.fs.writeFile(d_folder + '/sec_' + pos + '_out.png', base64Data, 'base64', function(err) {
-            res.send({ses:ses, sfn : req.body.sfn});
+            res.send({ses:ses});
         });
     } else {
         pkg.exec('cat $(find ' + d_folder + ' -name "sec_*_out.png" | sort -V) > ' + f_target + ' && rm -fr ' + d_folder, 
             function (error, stdout, stderr) {
-                res.send({status:'success', fn: 'up_' + ses, sfn : req.body.sfn});	
+                res.send({status:'success', fn: 'up_' + ses});	
             });
             
     }
