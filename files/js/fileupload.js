da7bbd3c2b6c4b6ecca07ddff1d129b0a92bb04f
b2blosangeles <b2blosangeles@gmail.com>
2018-10-22 13:35:48 -0700

    Update fileupload.js

--- files/js/fileupload.js
+++ files/js/fileupload.js
@@ -56,7 +56,7 @@
             $.ajax({
               type: "POST",
               url: '/api/upload.api',
-              data: {pos:'finished', ses: ses, fn : file.name},
+              data: {pos:'finished', ses: ses, sfn : file.name},
               success: function(data) {
                   (setting.done) ? setting.done(data) : '';
               },
