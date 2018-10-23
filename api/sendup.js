a7b3599d4e15c56ff545158c770fc7904201e5e3
b2blosangeles <b2blosangeles@gmail.com>
2018-10-23 05:16:23 -0700

    Update sendup.js

--- api/sendup.js
+++ api/sendup.js
@@ -1,16 +1,18 @@
-// res.sendFile('/tmp/' + req.query.fn);
-var fn = '/tmp/' + req.query.fn;
-pkg.fs.stat(fn, function(err, data) {
-  if (err) {  res.send(fn + ' does not exist'); }
-  else {
-      res.writeHead(200); 
-      var file = pkg.fs.createReadStream(fn);
-      file.pipe(res);
-      setTimeout(
-        function() {
-          file.destroy();
-          write404('timeout')
-        }, 30000
-      );
-    }
-});
+var fn = (req.query.fn) ? '/tmp/' + req.query.fn : '';
+if (!fn) res.send('Missing fn');
+else {
+  pkg.fs.stat(fn, function(err, data) {
+    if (err) {  res.send(fn + ' does not exist'); }
+    else {
+        res.writeHead(200); 
+        var file = pkg.fs.createReadStream(fn);
+        file.pipe(res);
+        setTimeout(
+          function() {
+            file.destroy();
+            write404('timeout')
+          }, 30000
+        );
+      }
+  });
+}
