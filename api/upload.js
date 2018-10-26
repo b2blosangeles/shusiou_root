d33b93b8c67d24ff3de7bf08009227733f798ab8
b2blosangeles <b2blosangeles@gmail.com>
2018-10-26 15:50:43 -0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,10 @@
 var pos = req.body.pos, ses = (!req.body.ses) ? (new Date().getTime()) : req.body.ses,
     base64Data = (req.body.data) ? req.body.data.replace(/^data:image\/png;base64,/, "") : null;
 
+if (!req.body.ses) {
+    res.send({status:'success', ses:ses});
+    return true;
+}
 /*
 if (pos > 3000) {
     res.send({status:'falure', message:'timeout', ses:ses});
