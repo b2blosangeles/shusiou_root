058375deea2c98da19abdd92f3bf668a44004e78
b2blosangeles <b2blosangeles@gmail.com>
2018-11-07 14:38:42 -0800

    Update DropBox.js

--- files/js/DropBox.js
+++ files/js/DropBox.js
@@ -1,3 +1,23 @@
-    var DropBox = function(setting) {
- 
+var DropBox = function(setting) {
+    this.holder = $('body')[0];
+    this.fileBuffer = null;
+    this.basket = $('#holder')[0];
+    this.init = function () {
+          var me  = this;
+          me.holder.ondragover = function () { 
+            this.className = 'hover'; return false; 
+          };
+          me.holder.ondragend = function () { 
+            this.className = ''; return false; 
+          };
+          me.holder.ondrop = function (e) {
+            console.log('====ondrop===');
+            this.className = '';
+            e.preventDefault();
+              fileBuffer = e.dataTransfer.files;
+              me.previewfiles(e.dataTransfer.files);
+              console.log('====ondrop===>e.dataTransfer.files');
+
+          }    
     }
+}
