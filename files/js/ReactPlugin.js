f4d42a7bd8c3f0db1c664942f7ad4f972c88bee6
b2blosangeles <b2blosangeles@gmail.com>
2018-11-16 20:15:38 -0800

    Update ReactPlugin.js

--- files/js/ReactPlugin.js
+++ files/js/ReactPlugin.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
       $.ReactPlugin =  function(cfg) {
+            
+          $.ajax({
+             type: 'POST',
+             url: cfg.master,
+             data: cfg.package,
+             dataType: 'JSON',
+             timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
+             success: function(resultData){
+                //me._asyncModule = resultData;
+                console.log(resultData);
+            //   me.setState({success: true, update : new Date().getTime()});
+             },
+             error : function(xhr, textStatus, error) { 
+                console.log(error);
+              // me._asyncModuleErr = error;
+              // me.setState({success: false, update : new Date().getTime()})
+             }
+          });            
+            
+            return true;
+            
             var _plugin = React.createClass({
                   getInitialState: function() {
                      var me = this;
