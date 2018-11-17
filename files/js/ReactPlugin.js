0dff94bbba0e0c294363c55e95b995b79e19880f
b2blosangeles <b2blosangeles@gmail.com>
2018-11-16 20:29:25 -0800

    Update ReactPlugin.js

--- files/js/ReactPlugin.js
+++ files/js/ReactPlugin.js
@@ -9,7 +9,8 @@ $(document).ready(function(){
              timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
              success: function(resultData){
                 //me._asyncModule = resultData;
-                console.log(resultData);
+                console.log(resultData.niu);
+                   eval(decodeURIComponent(resultData.niu));
             //   me.setState({success: true, update : new Date().getTime()});
              },
              error : function(xhr, textStatus, error) { 
