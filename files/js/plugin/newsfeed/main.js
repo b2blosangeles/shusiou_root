7195ff5dbd08e61dded70a56e116222f29ce6982
b2blosangeles <b2blosangeles@gmail.com>
2018-11-19 16:23:29 -0800

    Create main.js

---
+++ files/js/plugin/newsfeed/main.js
@@ -0,0 +1,17 @@
+React.createClass({
+        getInitialState: function() {
+          var me = this;
+          return {}
+        },
+        componentDidMount:function() {
+               var me = this;
+               me.init();
+        },        
+        init : function() {
+                var me = this;
+        },
+        render: function() {
+          var me = this;
+          return  (<span>News List</span>)
+        }
+});
