f52f133fc691e631dc5add6500e6230c66e5696a
b2blosangeles <b2blosangeles@gmail.com>
2018-11-19 17:08:07 -0800

    Update ReactPlugin.js

--- files/js/ReactPlugin.js
+++ files/js/ReactPlugin.js
@@ -1,25 +1,23 @@
 $(document).ready(function(){
       $.ReactPlugin =  function(cfg) {
-            (function(cfg) {
-                $.ajax({
-                   type: 'POST',
-                   url: cfg.master,
-                   data: cfg.extend,
-                   dataType: 'JSON',
-                   timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
-                   success: function(resultData){
+          $.ajax({
+             type: 'POST',
+             url: cfg.master,
+             data: cfg.extend,
+             dataType: 'JSON',
+             timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
+             success: function(resultData){
 
-                              eval(decodeURIComponent(resultData.inc));
-                              eval('var _asyncOBJ = ' + decodeURIComponent(resultData.master));
-                              ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
+                        eval(decodeURIComponent(resultData.inc));
+                        eval('var _asyncOBJ = ' + decodeURIComponent(resultData.master));
+                        ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
 
-                   },
-                   error : function(xhr, textStatus, error) { 
-                      console.log(error);
-                    // me._asyncModuleErr = error;
-                    // me.setState({success: false, update : new Date().getTime()})
-                   }
-                }); 
-             })(cfg);
+             },
+             error : function(xhr, textStatus, error) { 
+                console.log(error);
+              // me._asyncModuleErr = error;
+              // me.setState({success: false, update : new Date().getTime()})
+             }
+          });
       };
 });
