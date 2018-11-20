6aebdbab9ecd1aadb8e76ae7623b9803081b0cad
b2blosangeles <b2blosangeles@gmail.com>
2018-11-20 09:21:14 -0800

    Update ReactPlugin.js

--- files/js/ReactPlugin.js
+++ files/js/ReactPlugin.js
@@ -1,36 +1,38 @@
 $(document).ready(function(){
-      $.ReactPlugin =  function(cfg) {
- 
-          $.ajax({
-             type: 'POST',
-             url: cfg.master,
-             data: cfg.extend,
-             dataType: 'JSON',
-             timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
-             success: function(resultData){
-                   try {
-                        var _asyncOBJ;
-                        eval(decodeURIComponent(resultData.inc));
-                         
-                     //   eval('ReactDOM.render(' + decodeURIComponent(resultData.master));
-                        eval('ReactDOM.render(React.createElement(' + 
-                             decodeURIComponent(resultData.master).replace(/\;$/ig, '') + 
-                             ', null), cfg.viewPoint)');                         
-                       
-                        // eval('_asyncOBJ = ' + decodeURIComponent(resultData.master));
-                        // ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
-                   console.log('=C==>' + cfg.vv);
-                   console.log(resultData);
-                   } catch {
-                        console.log('something wrong ===!!!');
+      $.ReactPlugin =  function() {
+            var load = function(cfg) {
+                $.ajax({
+                   type: 'POST',
+                   url: cfg.master,
+                   data: cfg.extend,
+                   dataType: 'JSON',
+                   timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
+                   success: function(resultData){
+                         try {
+                              var _asyncOBJ;
+                              eval(decodeURIComponent(resultData.inc));
+
+                           //   eval('ReactDOM.render(' + decodeURIComponent(resultData.master));
+                              eval('ReactDOM.render(React.createElement(' + 
+                                   decodeURIComponent(resultData.master).replace(/\;$/ig, '') + 
+                                   ', null), cfg.viewPoint)');                         
+
+                              // eval('_asyncOBJ = ' + decodeURIComponent(resultData.master));
+                              // ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
+                         console.log('=C==>' + cfg.vv);
+                         console.log(resultData);
+                         } catch {
+                              console.log('something wrong ===!!!');
+                         }
+
+                   },
+                   error : function(xhr, textStatus, error) { 
+                      console.log(error);
+                    // me._asyncModuleErr = error;
+                    // me.setState({success: false, update : new Date().getTime()})
                    }
 
-             },
-             error : function(xhr, textStatus, error) { 
-                console.log(error);
-              // me._asyncModuleErr = error;
-              // me.setState({success: false, update : new Date().getTime()})
-             }
-          });
+                });
+          }
       };
 });
