889f038010dec79ab52e4c10925bfdcde2be381c
b2blosangeles <b2blosangeles@gmail.com>
2018-11-19 17:13:49 -0800

    Update ReactPlugin.js

--- files/js/ReactPlugin.js
+++ files/js/ReactPlugin.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
              dataType: 'JSON',
              timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
              success: function(resultData){
-
                         eval(decodeURIComponent(resultData.inc));
                         eval('var _asyncOBJ = ' + decodeURIComponent(resultData.master));
                         ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
+                   console.log('===>' + cfg.vv);
 
              },
              error : function(xhr, textStatus, error) { 
