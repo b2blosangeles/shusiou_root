033e7b5f7ffce0bcc5ff8dc3d0966a5b8d66bac7
b2blosangeles <b2blosangeles@gmail.com>
2018-11-19 18:11:35 -0800

    Update ReactPlugin.js

--- files/js/ReactPlugin.js
+++ files/js/ReactPlugin.js
@@ -9,8 +9,10 @@ $(document).ready(function(){
              timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
              success: function(resultData){
                         eval(decodeURIComponent(resultData.inc));
-                        eval('var ppp = "' + cfg.vv + '"; var _asyncOBJ = ' + decodeURIComponent(resultData.master));
-                        ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
+                        eval('var ppp = "' + cfg.vv + '"; var _asyncOBJ = ' + decodeURIComponent(resultData.master)
+                            + ' ReactDOM.render(React.createElement(_asyncOBJ, null), "' + cfg.viewPoint + '"); '
+                            );
+                     //   ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
                    console.log('=>==>' + cfg.vv);
                    console.log('-->--->' + ppp);
                    console.log(resultData);
