c5da01b941661c4af4edae254a5a452997b29597
b2blosangeles <b2blosangeles@gmail.com>
2018-11-19 16:42:58 -0800

    Update ReactPlugin.js

--- files/js/ReactPlugin.js
+++ files/js/ReactPlugin.js
@@ -1,23 +1,25 @@
 $(document).ready(function(){
       $.ReactPlugin =  function(cfg) {
-          $.ajax({
-             type: 'POST',
-             url: cfg.master,
-             data: cfg.extend,
-             dataType: 'JSON',
-             timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
-             success: function(resultData){
-                   (function() {
-                        eval(decodeURIComponent(resultData.inc));
-                        eval('var _asyncOBJ = ' + decodeURIComponent(resultData.master));
-                        ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
-                   })();
-             },
-             error : function(xhr, textStatus, error) { 
-                console.log(error);
-              // me._asyncModuleErr = error;
-              // me.setState({success: false, update : new Date().getTime()})
-             }
-          }); 
+            (function(cfg) {
+                $.ajax({
+                   type: 'POST',
+                   url: cfg.master,
+                   data: cfg.extend,
+                   dataType: 'JSON',
+                   timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
+                   success: function(resultData){
+
+                              eval(decodeURIComponent(resultData.inc));
+                              eval('var _asyncOBJ = ' + decodeURIComponent(resultData.master));
+                              ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
+
+                   },
+                   error : function(xhr, textStatus, error) { 
+                      console.log(error);
+                    // me._asyncModuleErr = error;
+                    // me.setState({success: false, update : new Date().getTime()})
+                   }
+                }); 
+             })(cfg);
       };
 });
