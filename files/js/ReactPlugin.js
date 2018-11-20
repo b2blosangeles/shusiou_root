e18473c289d61e68be8292bda8643c9a8a3473fb
b2blosangeles <b2blosangeles@gmail.com>
2018-11-20 09:04:52 -0800

    Update ReactPlugin.js

--- files/js/ReactPlugin.js
+++ files/js/ReactPlugin.js
@@ -14,11 +14,11 @@ $(document).ready(function(){
                          
                      //   eval('ReactDOM.render(' + decodeURIComponent(resultData.master));
                         eval('ReactDOM.render(React.createElement(' + decodeURIComponent(resultData.master).replace(/\;$/ig, '') + 
-                             ', null), document.getElementById("' + cfg.vv + '"))');                         
+                             ', null), ' + cfg.viewPoint + ')');                         
                        
                         // eval('_asyncOBJ = ' + decodeURIComponent(resultData.master));
                         // ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
-                   console.log('=B==>' + cfg.vv);
+                   console.log('=C==>' + cfg.vv);
                    console.log(resultData);
                    } catch {
                         console.log('something wrong ===!!!');
