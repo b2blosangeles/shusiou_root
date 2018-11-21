fbbfac48a97c14a46496cc137bfaaf6a91ab9b20
b2blosangeles <b2blosangeles@gmail.com>
2018-11-20 20:03:13 -0800

    Update main.jsx

--- files/js/plugin/newsfeed/main.jsx
+++ files/js/plugin/newsfeed/main.jsx
@@ -16,7 +16,7 @@ React.createClass({
                     <div className="container">
                         <div className="row">
                             <div className="col-sm-2">
-                                <button className="btn btn-success" onClick={me. goBackMyVideos.bind(me)}>
+                                <button className="btn btn-success" onClick={/*me. goBackMyVideos.bind(me)*/}>
                                  Go Back
                                 </button>
                             </div>
