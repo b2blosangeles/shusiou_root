f6dab95b592b4ff6484b1a4511493693640a8c5d
b2blosangeles <b2blosangeles@gmail.com>
2018-11-20 16:39:11 -0800

    Update ReactPlugin.js

--- files/js/ReactPlugin.js
+++ files/js/ReactPlugin.js
@@ -1,33 +1,19 @@
 $(document).ready(function(){
       $.ReactPlugin =  function(cfg) {
-            var me = this;
-            me.cfg = cfg;
-            console.log(JSON.parse(JSON.stringify(cfg)));
-            // me.cfg = JSON.parse(JSON.stringify(cfg));
-            this.load = function() {
-                  console.log('me.cfg.extend.main==>' + me.cfg.extend.main);
                 $.ajax({
                    type: 'POST',
-                   url: me.cfg.master,
-                   data: me.cfg.extend,
+                   url: cfg.master,
+                   data: cfg.extend,
                    dataType: 'JSON',
-                   timeout: (me.cfg.timeout) ? me.cfg.timeout : (6 * 1000),
+                   timeout: (cfg.timeout) ? cfg.timeout : (6 * 1000),
                    success: function(resultData){
                          try {
-                           //   var _asyncOBJ;
-                              eval('(function() { ' + decodeURIComponent(resultData.inc)+ 'ReactDOM.render(React.createElement(' + 
+                              eval('(function() { ' + decodeURIComponent(resultData.inc) + 
+                                   'ReactDOM.render(React.createElement(' + 
                                    decodeURIComponent(resultData.master).replace(/\;$/ig, '') + 
-                                   ', null), me.cfg.viewPoint); console.log(cfg.vv); ' + 
-                                   'console.log(resultData.masterUrl); ' + 
-                                   'console.log(cfg.extend.main); ' + 
-                                   ' console.log(unescape(resultData.master)); })()');                         
-
-                              // eval('_asyncOBJ = ' + decodeURIComponent(resultData.master));
-                              // ReactDOM.render(React.createElement(_asyncOBJ, null), cfg.viewPoint);
-                                //   console.log('DDD');
-                      //   console.log(resultData);
+                                   ', null), cfg.viewPoint); })()');                         
+    
                          } catch {
-                               console.log('EEE');
                               console.log('something wrong ===!!!');
                          }
 
@@ -37,8 +23,6 @@ $(document).ready(function(){
                     // me._asyncModuleErr = error;
                     // me.setState({success: false, update : new Date().getTime()})
                    }
-
                 });
-          }
       };
 });
