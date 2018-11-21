c468508362bbd4fe822ede9c1d9512d42d546dde
b2blosangeles <b2blosangeles@gmail.com>
2018-11-21 10:47:09 -0800

    Update main.jsx

--- files/js/plugin/newsfeed/main.jsx
+++ files/js/plugin/newsfeed/main.jsx
@@ -40,7 +40,7 @@ React.createClass({
 				<div className="jumbotron mt-1">
                             	{me.menu.map(function(m){
 					if (m.code === me.state.option) {
-						return('<span><_sectionA1/>==+===</span>')
+						return('<span>{me.module}</span>')
 					}
 				})}
                                 </div>
