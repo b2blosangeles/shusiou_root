90597b06e528524320f2b026a037fd81f192fddc
b2blosangeles <b2blosangeles@gmail.com>
2018-11-24 07:42:44 -0800

    Update and rename _sectionA1.jsx to sectionInventors.jsx

--- files/js/plugin/truefactors/sectionInventors.jsx
+++ files/js/plugin/truefactors/sectionInventors.jsx
@@ -1,4 +1,4 @@
-var _sectionA1 = React.createClass({
+var _sectionInventors = React.createClass({
         render: function() {
           var me = this;
           return  (<span>Section A - {new Date().getTime()} </span>)
