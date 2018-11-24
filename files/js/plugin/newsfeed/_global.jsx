c1bfabddb3fd2c41427353ae541030e7ba0a18f7
b2blosangeles <b2blosangeles@gmail.com>
2018-11-23 20:36:26 -0800

    Update _global.jsx

--- files/js/plugin/newsfeed/_global.jsx
+++ files/js/plugin/newsfeed/_global.jsx
@@ -3,5 +3,9 @@ var _global = {
 			{code: 'A1', caption: 'A1', module:'_sectionA1'},
 			{code: 'A2', caption: 'A2', module:'_sectionA1'},
 			{code: 'A3', caption: 'A3', module:'_sectionA1'}
-		]
+		],
+	options : {
+		inventors: 'Invenrtors',
+		investors: 'Investors'
+	}
 };
