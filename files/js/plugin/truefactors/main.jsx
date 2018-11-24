6bb20de8b9582d18be973d88da2986b0651d9105
b2blosangeles <b2blosangeles@gmail.com>
2018-11-24 06:43:09 -0800

    Update main.jsx

--- files/js/plugin/truefactors/main.jsx
+++ files/js/plugin/truefactors/main.jsx
@@ -33,27 +33,15 @@ React.createClass({
         render: function() {
           var me = this;
           return  (
-                    <div className="container">
-                        <div className="row">
-				<div className="col-sm-12"></div>
-                        </div>
-                        <div className="row">
-				<div className="col-sm-2">
-					<button className="btn btn-success btn-large" 
-						onClick={me.pickMenu.bind(me, 'inventors')}>
-						{me.roles.inventors}</button>
-				</div>
-				<div className="col-sm-8"></div>
-				<div className="col-sm-2">
-					<button className="btn btn-success btn-large pull-right" 
-						onClick={me.pickMenu.bind(me, 'investors')}>
-						{me.roles.investors}</button>
-				</div>
-                         </div>			  
-                        <div className="row">
+		<div className="container">
+			<div className="row"><div className="col-sm-12"></div></div>
+		</div>
+		<rolesMenu/>
+		<div className="container">
+			<div className="row">
 				<div className="jumbotron mt-1">{me.showBody()}</div>
-                         </div>			  
-                    </div>          
+			</div>			  
+		</div>          
           )
         }
 })
