3bcf3c0520729bbd8b17379da3c1b0cba6b17124
b2blosangeles <b2blosangeles@gmail.com>
2018-11-24 06:39:09 -0800

    Create rolesMenu.js

---
+++ files/js/plugin/truefactors/rolesMenu.js
@@ -0,0 +1,21 @@
+var rolesMenu = React.createClass({
+    render: function() {
+          var me = this;
+          return  (
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-2">
+                              <button className="btn btn-success btn-large" 
+                                onClick={me.pickMenu.bind(me, 'inventors')}>
+                                {me.roles.inventors}</button>
+                            </div>
+                            <div className="col-sm-8"></div>
+                            <div className="col-sm-2">
+                              <button className="btn btn-success btn-large pull-right" 
+                                onClick={me.pickMenu.bind(me, 'investors')}>
+                                {me.roles.investors}</button>
+                            </div>
+                         </div>	  
+                    </div>          
+          )
+}          
