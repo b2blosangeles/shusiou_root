7e5212a734870879a357b75c8a94b5d65d029fee
b2blosangeles <b2blosangeles@gmail.com>
2018-11-23 20:40:37 -0800

    Update _global.jsx

--- files/js/plugin/newsfeed/_global.jsx
+++ files/js/plugin/newsfeed/_global.jsx
@@ -1,5 +1,5 @@
 var _global = {
-    menu : [
+	menu : [
 			{code: 'A1', caption: 'A1', module:'_sectionA1'},
 			{code: 'A2', caption: 'A2', module:'_sectionA1'},
 			{code: 'A3', caption: 'A3', module:'_sectionA1'}
