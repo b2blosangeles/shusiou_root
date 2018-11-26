15b75e20cb84864bd1ef43167f56622411f299a9
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 05:15:18 -0800

    Update main.jsx

--- files/js/plugin/root/main.jsx
+++ files/js/plugin/root/main.jsx
@@ -7,9 +7,18 @@ React.createClass({
         render: function() {
           var me = this;
           return  (
-		<_asyncModule plugin={{extend: {
-			includes : [],
-			main : 'https://dev.shusiou.win/js/plugin/adbox/main.jsx'
+		<_asyncModule plugin={{extend: includes : [
+                           'https://dev.shusiou.win/js/plugin/truefactors/_global.jsx',
+                           'https://dev.shusiou.win/js/plugin/truefactors/_asyncModule.jsx',
+                           
+                           'https://dev.shusiou.win/js/plugin/truefactors/rolesMenu.jsx',
+                           'https://dev.shusiou.win/js/plugin/truefactors/secondMenu.jsx',
+                           
+                           'https://dev.shusiou.win/js/plugin/truefactors/sectionInventors.jsx',
+                           'https://dev.shusiou.win/js/plugin/truefactors/sectionInvestors.jsx'
+                         ],
+                         main : 'https://dev.shusiou.win/js/plugin/truefactors/main.jsx',
+                         cacheTime : 1000
 		}, 
 		master: '//master1_dev.shusiou.win/api/JSXhub.api'}} 
 		code="upload_video" parent={me} />
