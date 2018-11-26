b14076c75a2dc8ee504b60b20e5cfbb1531a29c5
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 11:16:29 -0800

    Create infopad.jsx

---
+++ files/js/plugin/salesForce/infopad.jsx
@@ -0,0 +1,14 @@
+React.createClass({
+        getInitialState: function() {
+		var me = this;
+          	return {}
+        }, 
+        render: function() {
+          var me = this;
+          return  (
+		<span>
+			infopad {new Date().getTime()} --> {Root.lib.test()}
+		</span>
+          )
+        }
+})
