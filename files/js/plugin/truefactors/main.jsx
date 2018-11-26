3113d976935dd47f330ab7061a614e68deeeeabb
b2blosangeles <b2blosangeles@gmail.com>
2018-11-25 23:09:28 -0800

    Update main.jsx

--- files/js/plugin/truefactors/main.jsx
+++ files/js/plugin/truefactors/main.jsx
@@ -40,7 +40,7 @@ React.createClass({
 			</div>
 
 			<div className="container">
-				<div className="row border border-secondary bg-light bg-warning rounded-bottom p-2 mt-1" 
+				<div className="row border border-secondary alert-success rounded-bottom p-2 mt-1" 
 					style={{'min-height' : '6em'}}>
 					{me.showBody()}
 				</div>			  
