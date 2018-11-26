cedf7fe63a6a57583f18f5e497c757c7736c1de0
b2blosangeles <b2blosangeles@gmail.com>
2018-11-25 22:41:01 -0800

    Update secondMenu.jsx

--- files/js/plugin/truefactors/secondMenu.jsx
+++ files/js/plugin/truefactors/secondMenu.jsx
@@ -7,7 +7,7 @@ var _secondMenu = React.createClass({
     render: function() {
           var me = this;
           return  (
-            <div className="secondMenu bg-secondary container-fluid" style={{'min-height' : '2.5em', color:'#fff'}}>
+            <div className="secondMenu bg-secondary container-fluid" style={{'height' : '2.5em', color:'#fff'}}>
 		<div className="container">
 			<div className="row">
 				<div className="col-sm-8 text-left site-logo pt-0">
