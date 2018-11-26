2262e094b161d4c6ee73508e0f40d89af547ca7a
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 10:50:29 -0800

    Update _commLib.jsx

--- files/js/plugin/root/_commLib.jsx
+++ files/js/plugin/root/_commLib.jsx
@@ -6,6 +6,12 @@ var _commLib = function () {
     this.getAuth = function() {
 		return (reactCookie.load('auth'))?reactCookie.load('auth'):{}
     }
+
+    this.test = function() {
+    	return '===From Test ===';
+    }
+	
+	
     this.inte_array = function (a, b) {
 	for(var i=0; i < a.length; i++) { if (b.indexOf(a[i]) !== -1) return true;}
 	return false;
