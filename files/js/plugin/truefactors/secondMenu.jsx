c611d079ce0401a0827e54f2a61f3c74e5cf1cb3
b2blosangeles <b2blosangeles@gmail.com>
2018-11-25 22:39:49 -0800

    Update secondMenu.jsx

--- files/js/plugin/truefactors/secondMenu.jsx
+++ files/js/plugin/truefactors/secondMenu.jsx
@@ -8,24 +8,26 @@ var _secondMenu = React.createClass({
           var me = this;
           return  (
             <div className="secondMenu bg-secondary container-fluid" style={{'min-height' : '2.5em', color:'#fff'}}>
-		<div className="row">
-			<div className="col-sm-8 text-left site-logo pt-0">
-				<ul className="nav">
-					<li className="nav-item">
-						<a className="nav-link active" href="#">Active</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">Link</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link disabled" href="#">Disabled</a>
-					</li>
-				</ul>
+		<div className="container">
+			<div className="row">
+				<div className="col-sm-8 text-left site-logo pt-0">
+					<ul className="nav">
+						<li className="nav-item">
+							<a className="nav-link active" href="#">Active</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Link</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link disabled" href="#">Disabled</a>
+						</li>
+					</ul>
+				</div>
+				<div className="col-sm-4 text-left site-logo pt-0">
+					<div className="float-sm-right pt-2">&#169; {new Date().getFullYear()} Polo Alto Project</div>
+				</div>
 			</div>
-			<div className="col-sm-4 text-left site-logo pt-0">
-				<div className="float-sm-right pt-2">&#169; {new Date().getFullYear()} Polo Alto Project</div>
-			</div>
-              	</div>			
+		</div>
             </div>          
           )
 	}          
