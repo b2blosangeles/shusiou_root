a9f825fe7cd20d5383da1f12c9bef759725ae426
b2blosangeles <b2blosangeles@gmail.com>
2018-11-25 22:06:48 -0800

    Create secondMenu.jsx

---
+++ files/js/plugin/truefactors/secondMenu.jsx
@@ -0,0 +1,17 @@
+var _secondMenu = React.createClass({
+    getInitialState: function() {
+		        var me = this;
+		        me.roles = _global.roles;
+          	return {}
+    },
+    render: function() {
+          var me = this;
+          return  (
+            <div className="bg-secondary container-fluid" style={{'min-height' : '2.5em', color:'#fff'}}>
+              <div className="container">
+                <div className="float-sm-right pt-2">&#169; {new Date().getFullYear()} San Ramon Project</div>
+              </div>			
+            </div>          
+          )
+	}          
+});
