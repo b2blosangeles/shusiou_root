8a848e19243ec6630ef96483e6f29ae58bd38d05
b2blosangeles <b2blosangeles@gmail.com>
2018-11-25 20:13:36 -0800

    Update rolesMenu.jsx

--- files/js/plugin/truefactors/rolesMenu.jsx
+++ files/js/plugin/truefactors/rolesMenu.jsx
@@ -13,7 +13,7 @@ var _rolesMenu = React.createClass({
           return  (
                     <div className="container">
                         <div className="row">
-				<div className="col-sm-4 text-left">
+				<div className="col-sm-4 text-left align-middle">
 					<h3 className="pull-left"><b>Polo Alto Project</b></h3>
 				</div>
 				<div className="col-sm-8 text-right">
