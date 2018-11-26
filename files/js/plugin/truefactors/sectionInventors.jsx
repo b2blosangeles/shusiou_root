a0a031173944a4bdd75e5f66c8c24b73025333ba
b2blosangeles <b2blosangeles@gmail.com>
2018-11-25 18:17:53 -0800

    Update sectionInventors.jsx

--- files/js/plugin/truefactors/sectionInventors.jsx
+++ files/js/plugin/truefactors/sectionInventors.jsx
@@ -2,54 +2,53 @@ var _sectionInventors = React.createClass({
         render: function() {
           var me = this;
           return  (<span>
-    <div classNme="container">
-      <div classNme="card-deck mb-3 text-center">
-        <div classNme="card mb-4 shadow-sm">
-          <div classNme="card-header">
-            <h4 classNme="my-0 font-weight-normal">Free</h4>
+    <div className="container">
+      <div className="card-deck mb-3 text-center">
+        <div className="card mb-4 shadow-sm">
+          <div className="card-header">
+            <h4 className="my-0 font-weight-normal">Free</h4>
           </div>
-          <div classNme="card-body">
-            <h1 classNme="card-title pricing-card-title">$0 <small classNme="text-muted">/ mo</small></h1>
-            <ul classNme="list-unstyled mt-3 mb-4">
+          <div className="card-body">
+            <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small></h1>
+            <ul className="list-unstyled mt-3 mb-4">
               <li>10 users included</li>
               <li>2 GB of storage</li>
               <li>Email support</li>
               <li>Help center access</li>
             </ul>
-            <button type="button" classNme="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+            <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
           </div>
         </div>
-        <div classNme="card mb-4 shadow-sm">
-          <div classNme="card-header">
-            <h4 classNme="my-0 font-weight-normal">Pro</h4>
+        <div className="card mb-4 shadow-sm">
+          <div className="card-header">
+            <h4 className="my-0 font-weight-normal">Pro</h4>
           </div>
-          <div classNme="card-body">
-            <h1 classNme="card-title pricing-card-title">$15 <small classNme="text-muted">/ mo</small></h1>
-            <ul classNme="list-unstyled mt-3 mb-4">
+          <div className="card-body">
+            <h1 className="card-title pricing-card-title">$15 <small className="text-muted">/ mo</small></h1>
+            <ul className="list-unstyled mt-3 mb-4">
               <li>20 users included</li>
               <li>10 GB of storage</li>
               <li>Priority email support</li>
               <li>Help center access</li>
             </ul>
-            <button type="button" classNme="btn btn-lg btn-block btn-primary">Get started</button>
+            <button type="button" className="btn btn-lg btn-block btn-primary">Get started</button>
           </div>
         </div>
-        <div classNme="card mb-4 shadow-sm">
-          <div classNme="card-header">
-            <h4 classNme="my-0 font-weight-normal">Enterprise</h4>
+        <div className="card mb-4 shadow-sm">
+          <div className="card-header">
+            <h4 className="my-0 font-weight-normal">Enterprise</h4>
           </div>
-          <div classNme="card-body">
-            <h1 classNme="card-title pricing-card-title">$29 <small classNme="text-muted">/ mo</small></h1>
-            <ul classNme="list-unstyled mt-3 mb-4">
+          <div className="card-body">
+            <h1 className="card-title pricing-card-title">$29 <small className="text-muted">/ mo</small></h1>
+            <ul className="list-unstyled mt-3 mb-4">
               <li>30 users included</li>
               <li>15 GB of storage</li>
               <li>Phone and email support</li>
               <li>Help center access</li>
             </ul>
-            <button type="button" classNme="btn btn-lg btn-block btn-primary">Contact us</button>
+            <button type="button" className="btn btn-lg btn-block btn-primary">Contact us</button>
           </div>
-        </div>
-      </div>                 
+              </div>               
                   </span>)
         }
 })
