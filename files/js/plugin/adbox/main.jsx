de493c0c09bd809268bf5260245416ee77a5f0b5
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 14:01:23 -0800

    Update main.jsx

--- files/js/plugin/adbox/main.jsx
+++ files/js/plugin/adbox/main.jsx
@@ -9,11 +9,27 @@ React.createClass({
         },	
         render: function() {
           var me = this;
-          return  (
-		<span>
-			adx {new Date().getTime()} --> {Root.lib.test('adx')} ***> {me.props.data.role} || {me.props.data.v}
-			  || {me.props.data.sk}
-		</span>
-          )
+		if (me.props.data.sk === '1')
+			  return  (
+				<span>
+
+					adx {new Date().getTime()} --> {Root.lib.test('adx')} ***> {me.props.data.role} || {me.props.data.v}
+					  || {me.props.data.sk}
+				</span>
+			  )
+		else if (me.props.data.sk === '2')
+			return  (
+				<span>
+
+					solution 2
+				</span>
+			  )
+		else 
+			return  (
+				<span>
+
+					solution 3
+				</span>
+			  )			
         }
 })
