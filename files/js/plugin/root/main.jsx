0f6184bde369c7d29b55ca0eefd5dce5b1924981
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 05:17:59 -0800

    Update main.jsx

--- files/js/plugin/root/main.jsx
+++ files/js/plugin/root/main.jsx
@@ -7,20 +7,22 @@ React.createClass({
         render: function() {
           var me = this;
           return  (
-		<_asyncModule plugin={{extend: includes : [
-                           'https://dev.shusiou.win/js/plugin/truefactors/_global.jsx',
-                           'https://dev.shusiou.win/js/plugin/truefactors/_asyncModule.jsx',
-                           
-                           'https://dev.shusiou.win/js/plugin/truefactors/rolesMenu.jsx',
-                           'https://dev.shusiou.win/js/plugin/truefactors/secondMenu.jsx',
-                           
-                           'https://dev.shusiou.win/js/plugin/truefactors/sectionInventors.jsx',
-                           'https://dev.shusiou.win/js/plugin/truefactors/sectionInvestors.jsx'
-                         ],
-                         main : 'https://dev.shusiou.win/js/plugin/truefactors/main.jsx',
-                         cacheTime : 1000
-		}, 
-		master: '//master1_dev.shusiou.win/api/JSXhub.api'}} 
+		<_asyncModule plugin={{
+			extend: {
+				  includes : [
+					   'https://dev.shusiou.win/js/plugin/truefactors/_global.jsx',
+					   'https://dev.shusiou.win/js/plugin/truefactors/_asyncModule.jsx',
+
+					   'https://dev.shusiou.win/js/plugin/truefactors/rolesMenu.jsx',
+					   'https://dev.shusiou.win/js/plugin/truefactors/secondMenu.jsx',
+
+					   'https://dev.shusiou.win/js/plugin/truefactors/sectionInventors.jsx',
+					   'https://dev.shusiou.win/js/plugin/truefactors/sectionInvestors.jsx'
+					 ],
+					main : 'https://dev.shusiou.win/js/plugin/truefactors/main.jsx',
+                         		cacheTime : 1000
+				}, 
+				master: '//master1_dev.shusiou.win/api/JSXhub.api'}} 
 		code="sectionInventors" parent={me} />
           )
         }
