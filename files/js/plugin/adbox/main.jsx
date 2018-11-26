9d04acc6d3f2186cf1f0f711e2c9bf8c73dc7dc7
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 10:51:22 -0800

    Update main.jsx

--- files/js/plugin/adbox/main.jsx
+++ files/js/plugin/adbox/main.jsx
@@ -7,7 +7,7 @@ React.createClass({
           var me = this;
           return  (
 		<span>
-			adx {new Date().getTime()}
+			adx {new Date().getTime()} --> {Root.test()}
 		</span>
           )
         }
