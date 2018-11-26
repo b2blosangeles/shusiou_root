65e8f546b7a4dc2783c0ad563179d73743b2c5c9
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 12:37:15 -0800

    Update main.jsx

--- files/js/plugin/adbox/main.jsx
+++ files/js/plugin/adbox/main.jsx
@@ -2,16 +2,7 @@ React.createClass({
         getInitialState: function() {
 		var me = this;
           	return {}
-        },
-        componentDidMount:function() {
-               var me = this;
-		console.log('---componentDidMount---');
-		console.log(me.props);
-        }, 
-        componentDidUpdate:function() {
-               var me = this;
-		console.log('---componentDidUpdate---');
-        }, 	
+        },	
         render: function() {
           var me = this;
           return  (
