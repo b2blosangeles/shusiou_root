dca56730220e5eecb874eee2e40794d62bcd79b4
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 13:28:44 -0800

    Update main.jsx

--- files/js/plugin/truefactors/main.jsx
+++ files/js/plugin/truefactors/main.jsx
@@ -20,7 +20,7 @@ React.createClass({
 							main : 'https://dev.shusiou.win/js/plugin/adbox/main.jsx'
 						}, 
 						master: '//master1_dev.shusiou.win/api/JSXhub.api'}} 
-					info={me.state.role}
+					data={{role: me.state.role}}
 					parent={me} />
 	},
         init : function(code) {
