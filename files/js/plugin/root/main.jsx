4532c689ac930c359ea8a35dad2d156d896987c9
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 05:09:32 -0800

    Create main.jsx

---
+++ files/js/plugin/root/main.jsx
@@ -0,0 +1,15 @@
+
+React.createClass({
+        getInitialState: function() {
+		var me = this;
+          	return {}
+        }, 
+        render: function() {
+          var me = this;
+          return  (
+		<span>
+			adx {new Date().getTime()}
+		</span>
+          )
+        }
+})
