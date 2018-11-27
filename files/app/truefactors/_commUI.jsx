8edba19f668ecf07fb53d68ee7bfd596d6546bd3
b2blosangeles <b2blosangeles@gmail.com>
2018-11-27 07:58:52 -0800

    Update _commUI.jsx

--- files/app/truefactors/_commUI.jsx
+++ files/app/truefactors/_commUI.jsx
@@ -1,7 +1,6 @@
 var _commUI = function () {
     this.videoBox = 
         function() { 
-            return (<span>testA</span>)
              return   React.createClass({
                     render : function () { 
                         return (<span>test</span>)
