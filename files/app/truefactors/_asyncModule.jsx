da9a981f51f92dac6fd23d6d43e26b92b08b5ee9
b2blosangeles <b2blosangeles@gmail.com>
2018-11-27 06:40:35 -0800

    Update _asyncModule.jsx

--- files/app/truefactors/_asyncModule.jsx
+++ files/app/truefactors/_asyncModule.jsx
@@ -53,7 +53,7 @@ try {
 							var Root = (!_commLib) ? {} : 
 							{
 								lib	: new _commLib(),
-								commUI	: _commUI,
+								commUI	: new _commUI(),
 								global	: _global
 							};
 						} 
