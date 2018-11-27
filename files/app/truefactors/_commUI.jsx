ba57b122ff25bf428d9f5093d03dc908a00e2657
b2blosangeles <b2blosangeles@gmail.com>
2018-11-27 14:27:12 -0800

    Update _commUI.jsx

--- files/app/truefactors/_commUI.jsx
+++ files/app/truefactors/_commUI.jsx
@@ -3,7 +3,7 @@ var _commUI = {
                     render : function () { 
                         var me = this;
                         return (
-                            <div className="col-sm-3">
+                            <div className="col-sm-3 p-1">
                                 <div className="shadow border border-secondary bg-cellBox rounded p-2 m-1" 
                                     style={{'min-height' : '9em'}}>
                                     {me.props.data}
