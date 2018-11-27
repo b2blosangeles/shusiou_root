f9cf8307d3a9ad06e9e5e641b9133bb7936ae0cc
b2blosangeles <b2blosangeles@gmail.com>
2018-11-27 14:54:16 -0800

    Update main.jsx

--- files/app/truefactors/root/main.jsx
+++ files/app/truefactors/root/main.jsx
@@ -123,7 +123,7 @@ React.createClass({
 						<div className="row ">
 						<div className="col-sm-12 p-1">	
 							<div className="border border-secondary bg-secondary 
-									text-center align-bottom rounded m-1 mx-3" 
+									text-center align-bottom rounded p-1 m-1 mx-3" 
 								style={{'height' : '2.5em', 'color' : '#fff'}}>
 								&#169; {new Date().getFullYear()} Polo Alto Project
 							</div>
