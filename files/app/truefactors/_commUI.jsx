79006a0395426e12bff323eba17d3cb7ac46c5e3
b2blosangeles <b2blosangeles@gmail.com>
2018-11-27 08:01:44 -0800

    Update _commUI.jsx

--- files/app/truefactors/_commUI.jsx
+++ files/app/truefactors/_commUI.jsx
@@ -1,10 +1,10 @@
 var _commUI = function () {
-    this.videoBox = 
-        function() { 
-             return   React.createClass({
+    this.videoBox = function() { 
+            var _videoBox = React.createClass({
                     render : function () { 
                         return (<span>test</span>)
                     }
-                })
+                });
+            return <_videoBox>
         }
 };
