8ce885058ff2ef8817d7445808398bec71eddd82
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 21:19:52 -0800

    Update main.jsx

--- files/js/plugin/truefactors/main/main.jsx
+++ files/js/plugin/truefactors/main/main.jsx
@@ -64,36 +64,28 @@ React.createClass({
 							
 							<div className="col-sm-3 p-1 pull-left">
 								<div className="border border-secondary alert-light 
-										rounded p-2" 
-									style={{'min-height' : '8em', 'margin' : '2px'}}>
+										rounded p-2 m-1" 
+									style={{'min-height' : '8em'}}>
 									{me.asyncAdbox('2')}
 								</div>
 							</div>
 
 							<div className="col-sm-3 p-1 pull-left">
 								<div className="border border-secondary alert-light 
-										rounded p-2" 
-									style={{'min-height' : '8em', 'margin' : '2px'}}>
+										rounded p-2 m-1" 
+									style={{'min-height' : '8em'}}>
 									{me.asyncAdbox('2')}
 								</div>
 							</div>
 
 							<div className="col-sm-3 p-1 pull-left">
 								<div className="border border-secondary alert-light 
-										rounded p-2" 
-									style={{'min-height' : '8em', 'margin' : '2px'}}>
+										rounded p-2 m-1" 
+									style={{'min-height' : '8em'}}>
 									{me.asyncAdbox('2')}
 								</div>
 							</div>
-
-							<div className="col-sm-3 p-1">
-								<div className="border border-secondary alert-light 
-										rounded p-2" 
-									style={{'min-height' : '8em', 'margin' : '2px'}}>
-									{me.asyncAdbox('2')}
-								</div>
-							</div>
-							
+						
 							<div className="container">
 									<div className="row">
 										<div className="col-sm-3 p-1">
