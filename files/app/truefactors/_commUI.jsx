b21f4487a08a6368b5ec79afa8d3550bb2fa0e6f
b2blosangeles <b2blosangeles@gmail.com>
2018-11-27 14:03:50 -0800

    Update _commUI.jsx

--- files/app/truefactors/_commUI.jsx
+++ files/app/truefactors/_commUI.jsx
@@ -11,5 +11,18 @@ var _commUI = {
                             </div>                        
                         )
                     }
+                }),
+      infoBox : React.createClass({
+                    render : function () { 
+                        var me = this;
+                        return (
+                            <div className="col-sm-12 p-1">
+                                <div className="shadow border border-secondary bg-cellBox rounded p-2 m-1" 
+                                    style={{'min-height' : '9em'}}>
+                                    {me.props.data}
+                                </div>
+                            </div>                        
+                        )
+                    }
                 })
 };
