65e98aa22120473df21aff4a82db2047a84f5e64
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 20:33:37 -0800

    Update main.jsx

--- files/js/plugin/truefactors/main/main.jsx
+++ files/js/plugin/truefactors/main/main.jsx
@@ -127,8 +127,8 @@ React.createClass({
 			<div className="container">
 				<div className="row">
 					<div className="col-sm-12 p-1">
-						<div className="row border border-secondary bg-secondary p-2" 
-							style={{'min-height' : '2em', 'text-align' : 'center'}}>
+						<div className="bg-secondary p-2" 
+							style={{'min-height' : '2em', 'align' : 'center', 'color' : '#fff'}}>
 							&#169; {new Date().getFullYear()} Polo Alto Project
 						</div>
 					</div>
