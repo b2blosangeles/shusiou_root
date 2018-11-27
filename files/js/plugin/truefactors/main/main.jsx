35bf50324a3127f53366e046e7d31700754870e3
b2blosangeles <b2blosangeles@gmail.com>
2018-11-26 20:07:15 -0800

    Update main.jsx

--- files/js/plugin/truefactors/main/main.jsx
+++ files/js/plugin/truefactors/main/main.jsx
@@ -68,14 +68,14 @@ React.createClass({
 			  
 			<div className="container">
 				<div className="row">
-					<div className="col-sm-6 p-1">
+					<div className="col-sm-4 p-1">
 						<div className="row border border-secondary alert-info 
 								rounded-bottom p-2 pull-left" 
 							style={{'min-height' : '8em', 'margin-right' : '2px'}}>
 							{me.asyncAdbox('2')}
 						</div>
 					</div>
-					<div className="col-sm-6 p-1">
+					<div className="col-sm-4 p-1">
 						<div className="row border border-secondary alert-info 
 								rounded-bottom p-2" 
 							style={{'min-height' : '8em', 
@@ -84,7 +84,7 @@ React.createClass({
 							{me.asyncAdbox('2')}
 						</div>
 					</div>
-					<div className="col-sm-6 p-1">
+					<div className="col-sm-4 p-1">
 						<div className="row border border-secondary alert-info 
 								rounded-bottom p-2 pull-right" 
 							style={{'min-height' : '8em', 'margin-left' : '2px'}}>
