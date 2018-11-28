af1fb510046132bf427f01c9b1140c07e5768cce
b2blosangeles <b2blosangeles@gmail.com>
2018-11-27 21:58:06 -0800

    Update main.jsx

--- files/app/truefactors/myVideos/main.jsx
+++ files/app/truefactors/myVideos/main.jsx
@@ -7,11 +7,18 @@ React.createClass({
 		var _videoBox = Root.commUI.videoBox;
 		return <_videoBox data={v} />
 	},
+	infoBox : function(data, classCFG) {
+		var _infoBox = Root.commUI.infoBox;
+		return <_infoBox data={data} class={classCFG} />
+	},
         render: function() {
           var me = this;
 	  var list = Root.lib.getNumberList(10);
           return  (
 		<div className="container">
+			<div className="row ">
+			  {me.infoBox('', {type: 'success'})}
+			</div>
 			<div className="row ">
 				{list.map(function(m) {
 					return me.videoBox('v' + m)
