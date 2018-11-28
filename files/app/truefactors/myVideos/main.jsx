83a553030441b9b047dabe7a0dd277751878d609
b2blosangeles <b2blosangeles@gmail.com>
2018-11-27 21:44:15 -0800

    Update main.jsx

--- files/app/truefactors/myVideos/main.jsx
+++ files/app/truefactors/myVideos/main.jsx
@@ -2,13 +2,23 @@ React.createClass({
         getInitialState: function() {
 		var me = this;
           	return {}
-        }, 
+        },
+	videoBox : function(v) {
+		var _videoBox = Root.commUI.videoBox;
+		return <_videoBox data={v} />
+	},
         render: function() {
           var me = this;
+	  var list = Root.lib.getNumberList(10);
           return  (
-		<span>
-			  myVideos
-		</span>
-          )
+		<div className="container">
+			<div className="row ">
+			{me.showBody()}	
+				<hr/>
+			{list.map(function(m) {
+				return me.videoBox('v' + m)
+			})}							
+			</div>
+		</div>)
         }
 })
