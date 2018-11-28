96390582054ef62d4bb6abb387555f41ace96bdb
b2blosangeles <b2blosangeles@gmail.com>
2018-11-28 11:45:10 -0800

    Update main.jsx

--- files/app/truefactors/errPage/main.jsx
+++ files/app/truefactors/errPage/main.jsx
@@ -3,16 +3,19 @@ React.createClass({
 		var me = this;
           	return {}
         },
-	infoBox : function(data, classCFG) {
+	infoBox : function(data, setting) {
 		var _infoBox = Root.commUI.infoBox;
-		return <_infoBox data={data} class={classCFG} />
+		return <_infoBox data={data} setting={setting} />
 	},
         render: function() {
           var me = this;
           return  (
 		<div className="container">
 			<div className="row ">
-			  {me.infoBox('Error: Empty Module!!!', {type: 'danger'})}
+			  {me.infoBox('Error: Empty Module!!!', {
+					type: 'danger', 
+					style: {'min-height' : '36em'}
+				})     
 			</div>
 		</div>)
         }
