d4432bd63909516aee8a72ddb9e182b32ce56d56
b2blosangeles <b2blosangeles@gmail.com>
2018-11-28 13:47:56 -0800

    Update main.jsx

--- files/app/truefactors/root/main.jsx
+++ files/app/truefactors/root/main.jsx
@@ -13,7 +13,7 @@ React.createClass({
 						main : 'https://dev.shusiou.win/app/truefactors/inventors/main.jsx'
 					}, 
 					master: '//master1_dev.shusiou.win/api/JSXhub.api'
-				}, ''
+				}, '', me
 			)
 		}		
 		
