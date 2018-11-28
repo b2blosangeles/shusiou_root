d018b767dd235e2d6b1ce492c5868f62bc14a203
b2blosangeles <b2blosangeles@gmail.com>
2018-11-28 13:09:23 -0800

    Delete sectionInvestors.jsx

--- files/app/truefactors/root/sectionInvestors.jsx
+++
@@ -1,6 +0,0 @@
-var _sectionInvestors = React.createClass({
-        render: function() {
-          var me = this;
-          return  (<span>Section  Investors  - {new Date().getTime()} </span>)
-        }
-})
