6b0a9cefa96a99433440ad4105bcf2dee9929fae
b2blosangeles <b2blosangeles@gmail.com>
2018-11-28 14:24:43 -0800

    Update _commUI.jsx

--- files/app/truefactors/_commUI.jsx
+++ files/app/truefactors/_commUI.jsx
@@ -6,10 +6,15 @@ var _commUI = {
     cellBox : React.createClass({
                     render : function () { 
                         var me = this;
+                        var classType = (!me.props.setting || !me.props.setting.type) ? 'info' : me.props.setting.type;
+                        var shadow = (!me.props.setting || !me.props.setting.noshadow) ? ' shadow ' : '';
+                        var className = shadow + 
+                                    'border border-' + classType + ' alert-' + classType + ' ' +
+                                    'rounded p-2 m-1';
+			var style = (!me.props.setting || !me.props.setting.style) ? {'min-height' : '9em'} : me.props.setting.style;
                         return (
                             <div className="col-sm-3 p-1">
-                                <div className="shadow border border-secondary bg-cellBox rounded p-2 m-1" 
-                                    style={{'min-height' : '9em'}}>
+                                <div className={className} style={style} >
                                      {(typeof me.props.data === 'string') ? (<span dangerouslySetInnerHTML={{__html: me.props.data}}/>)
                                     : me.props.data}
                                 </div>
@@ -28,8 +33,7 @@ var _commUI = {
                         var style = (!me.props.setting || !me.props.setting.style) ? {'min-height' : '2em'} : me.props.setting.style;
                         return (
                             <div className="col-sm-12 p-1">
-                                <div className={className} 
-                                    style={style} >
+                                <div className={className} style={style} >
                                     {(typeof me.props.data === 'string') ? (<span dangerouslySetInnerHTML={{__html: me.props.data}}/>)
                                     : me.props.data}
                                 </div>
