e09f1109a03fea92abdd72d18bcaf794c853ee4d
b2blosangeles <b2blosangeles@gmail.com>
2018-11-28 08:32:18 -0800

    Update main.jsx

--- files/app/truefactors/main.jsx
+++ files/app/truefactors/main.jsx
@@ -6,6 +6,8 @@ React.createClass({
         }, 
         render: function() {
         	var me = this;
+		$.ReactPlugin_objId = (!$.ReactPlugin_objId || $.ReactPlugin_objId > 1000000) ? 1 : ($.ReactPlugin_objId + 1);
+		var _objId = $.ReactPlugin_objId + '-' + new Date().getTime();
 		return  (
 			<_asyncModule plugin={{
 			extend: {
@@ -21,7 +23,7 @@ React.createClass({
 				   cacheTime : 1000
 				}, 
 				master: '//master1_dev.shusiou.win/api/JSXhub.api'}} 
-		parent={me} objId={$.ReactPlugin_objId + '_' + new Date().getTime()} />
+		parent={me} objId={_objId} />
 		)
         }
 })
