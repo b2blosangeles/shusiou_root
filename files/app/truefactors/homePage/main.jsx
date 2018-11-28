efc15561ffba7a332551e3858d63041c7665208a
b2blosangeles <b2blosangeles@gmail.com>
2018-11-28 12:42:30 -0800

    Update main.jsx

--- files/app/truefactors/homePage/main.jsx
+++ files/app/truefactors/homePage/main.jsx
@@ -3,16 +3,12 @@ React.createClass({
 		var me = this;
           	return {}
         },
-	infoBox : function(data, setting) {
-		var _infoBox = Root.commUI.infoBox;
-		return <_infoBox data={data} setting={setting} />
-	},
         render: function() {
           var me = this;
           return  (
 		<div className="container">
 			<div className="row ">
-			  {me.infoBox('Home <br> Page', 
+			  {Root.commUI.show('infoBox', me, 'Home <br> Page', 
 				{
 					type: 'success', 
 					style: {'min-height' : '28em'}
