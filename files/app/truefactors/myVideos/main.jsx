ca88baec594eaac5bab1f5bd05a0ba63b8f2fa81
b2blosangeles <b2blosangeles@gmail.com>
2018-11-27 21:16:16 -0800

    Create main.jsx

---
+++ files/app/truefactors/myVideos/main.jsx
@@ -0,0 +1,14 @@
+React.createClass({
+        getInitialState: function() {
+		var me = this;
+          	return {}
+        }, 
+        render: function() {
+          var me = this;
+          return  (
+		<span>
+			  myVideos
+		</span>
+          )
+        }
+})
