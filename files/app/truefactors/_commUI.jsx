5274b30ab1b42f29669c4864c5ebaaa92596ddf4
b2blosangeles <b2blosangeles@gmail.com>
2018-11-28 12:15:51 -0800

    Update _commUI.jsx

--- files/app/truefactors/_commUI.jsx
+++ files/app/truefactors/_commUI.jsx
@@ -24,7 +24,8 @@ var _commUI = {
                         return (
                             <div className="col-sm-12 p-1">
                                 <div className={className} 
-                                    style={style} dangerouslySetInnerHTML={{__html: me.props.data}}>
+                                    style={style} >
+                                    <span dangerouslySetInnerHTML={{__html: me.props.data}}/>
                                     {/*me.props.data*/}
                                 </div>
                             </div>                        
