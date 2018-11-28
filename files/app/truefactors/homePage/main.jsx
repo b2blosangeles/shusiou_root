4127b28f026a6d7be99ed9bbde7c5a32c96e88c1
b2blosangeles <b2blosangeles@gmail.com>
2018-11-27 22:25:36 -0800

    Create main.jsx

---
+++ files/app/truefactors/homePage/main.jsx
@@ -0,0 +1,29 @@
+eact.createClass({
+        getInitialState: function() {
+		var me = this;
+          	return {}
+        },
+	videoBox : function(v) {
+		var _videoBox = Root.commUI.videoBox;
+		return <_videoBox data={v} />
+	},
+	infoBox : function(data, classCFG) {
+		var _infoBox = Root.commUI.infoBox;
+		return <_infoBox data={data} class={classCFG} />
+	},
+        render: function() {
+          var me = this;
+	  var list = Root.lib.getNumberList(10);
+          return  (
+		<div className="container">
+			<div className="row ">
+			  {me.infoBox('Test', {type: 'success'})}
+			</div>
+			<div className="row ">
+				{list.map(function(m) {
+					return me.videoBox('v' + m)
+				})}							
+			</div>
+		</div>)
+        }
+})
