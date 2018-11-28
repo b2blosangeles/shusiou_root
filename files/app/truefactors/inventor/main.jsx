9d2657c6db01b1ff132ff5c31f590809a2acffe1
b2blosangeles <b2blosangeles@gmail.com>
2018-11-28 12:58:15 -0800

    Create main.jsx

---
+++ files/app/truefactors/inventor/main.jsx
@@ -0,0 +1,29 @@
+React.createClass({
+        getInitialState: function() {
+		var me = this;
+          	return {}
+        },
+	cellBox : function(v) {
+		var _cellBox = Root.commUI.cellBox;
+		return <_cellBox data={v} />
+	},
+	infoBox : function(data, setting) {
+		var _infoBox = Root.commUI.infoBox;
+		return <_infoBox data={data} setting={setting} />
+	},
+        render: function() {
+          var me = this;
+	  var list = Root.lib.getNumberList(10);
+          return  (
+		<div className="container">
+			<div className="row ">
+			  {me.infoBox('Test', {type: 'success'})}
+			</div>
+			<div className="row ">
+				{list.map(function(m) {
+					return me.cellBox('<b>v' + m + '</b>')
+				})}							
+			</div>
+		</div>)
+        }
+})
