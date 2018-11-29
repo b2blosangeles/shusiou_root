4d807363a67830addc60889677975072183f1a29
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 10:39:27 -0800

    Update _commUI.jsx

--- files/app/truefactors/_commUI.jsx
+++ files/app/truefactors/_commUI.jsx
@@ -47,8 +47,8 @@ var _commUI = {
                         var classType = (!me.props.setting || !me.props.setting.type) ? 'info' : me.props.setting.type;
                         var shadow = (!me.props.setting || !me.props.setting.noshadow) ? ' shadow ' : '';
                         var className = shadow + 
-                                   ((!me.props.setting || !me.props.setting.class) ? (
-					   'border border-' + classType + ' alert-' + classType + ' ') : me.props.setting.class) + ' ' +
+                                   ((!me.props.setting || !me.props.setting.class) ? 
+				    'border border-' + classType + ' alert-' + classType + ' ') : me.props.setting.class) + ' ' +
 			    	    ((!me.props.setting || !me.props.setting.rounded) ? '' : ' rounded') +
                                     ' p-2 m-1';
                         var style = (!me.props.setting || !me.props.setting.style) ? {'min-height' : '2em'} : me.props.setting.style;
