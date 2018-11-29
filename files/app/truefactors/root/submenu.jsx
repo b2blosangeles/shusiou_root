875fdb166f4b885c4b12f7a9ad76e6080faeb8a1
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 13:12:33 -0800

    Update submenu.jsx

--- files/app/truefactors/root/submenu.jsx
+++ files/app/truefactors/root/submenu.jsx
@@ -20,21 +20,21 @@ var _subMenu = React.createClass({
 				<div className="col-sm-12 text-left site-logo pt-0">
 					<ul className="nav">
 					{(me.props.parent.state.menuOption !== '') ?
-							(<span>
+						(<span>
+						<li className="nav-item">
+							<a className="nav-link active" 
+								onClick={me.clickOption.bind(me, '')}
+								href="JavaScript: void(0)">Home</a>
+						</li></span>)
+						:
+						(<span>
 							<li className="nav-item">
-								<a className="nav-link active" 
-									onClick={me.clickOption.bind(me, '')}
-									href="JavaScript: void(0)">Home</a>
-							</li></span>)
-							:
-							(<span>
-								<li className="nav-item">
-									<a className="nav-link disabled" 
-									href="JavaScript: void(0)">
-									<span style={{color:'yellow'}}>
-										Home</span></a>
-								</li></span>)
-						})}
+								<a className="nav-link disabled" 
+								href="JavaScript: void(0)">
+								<span style={{color:'yellow'}}>
+									Home</span></a>
+							</li></span>)}
+						
 					{menu.map(function(m){ 	
 						return (me.props.parent.state.menuOption !== m.code) ?
 								(<span>
