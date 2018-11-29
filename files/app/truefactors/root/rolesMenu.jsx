c15cf85cc5fd5601511f165b203c7d6cbd42b3a1
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 06:44:10 -0800

    Update rolesMenu.jsx

--- files/app/truefactors/root/rolesMenu.jsx
+++ files/app/truefactors/root/rolesMenu.jsx
@@ -18,7 +18,7 @@ var _rolesMenu = React.createClass({
 					<div className="container">
 					<div className="row">
 						<div className="col-sm-3 text-left site-logo pt-2">
-							<img src="/app/truefactors/imgs/platoplan_logo.png" height="60"/>
+							<img src="/app/truefactors/imgs/platoplan_logo.png" height="68"/>
 							{/*--- https://flamingtext.com/net-fu/dynamic.cgi?script=colored2-logo&text=Inventions&fontname=porky%27s ---*/}
 						</div>
 						<div className="col-sm-9 text-right pt-2">
