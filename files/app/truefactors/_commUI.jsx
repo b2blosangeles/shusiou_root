d64063323346fb3efaf5eec56dd26cca8c5015e8
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 10:11:30 -0800

    Update _commUI.jsx

--- files/app/truefactors/_commUI.jsx
+++ files/app/truefactors/_commUI.jsx
@@ -41,6 +41,25 @@ var _commUI = {
                         )
                     }
                 }),
+      documentPage : React.createClass({
+                    render : function () { 
+                        var me = this;
+                        var classType = (!me.props.setting || !me.props.setting.type) ? 'info' : me.props.setting.type;
+                        var shadow = (!me.props.setting || !me.props.setting.noshadow) ? ' shadow ' : '';
+                        var className = shadow + 
+                                    'border border-' + classType + ' alert-' + classType + ' ' +
+                                    'rounded p-2 m-1';
+                        var style = (!me.props.setting || !me.props.setting.style) ? {'min-height' : '2em'} : me.props.setting.style;
+                        return (
+                            <div className="col-sm-12 p-1">
+                                <div className={className} style={style} >
+                                    {(typeof me.props.data === 'string') ? (<span dangerouslySetInnerHTML={{__html: me.props.data}}/>)
+                                    : me.props.data}
+                                </div>
+                            </div>                        
+                        )
+                    }
+                }),	
 	spinner : React.createClass({
                     render : function () { 
                         var me = this;
