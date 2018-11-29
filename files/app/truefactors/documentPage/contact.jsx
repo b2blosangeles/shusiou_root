3d5d0beba8e4b14ead36a20bc53e2876ad74ea5b
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 14:44:31 -0800

    Update contact.jsx

--- files/app/truefactors/documentPage/contact.jsx
+++ files/app/truefactors/documentPage/contact.jsx
@@ -4,6 +4,62 @@ var _contact = React.createClass({
 	return {};
     },
     render: function() {
-	return  (<span>Contact Us</span>)
+	return  (<span>
+			<h3>Contact Us</h3>
+			<p>
+			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
+			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
+			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
+			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
+			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
+			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			</p>
+			
+			<p>
+			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
+			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
+			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
+			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
+			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
+			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			</p>
+			
+			<p>
+			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
+			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
+			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
+			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
+			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
+			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			</p>
+
+			<p>
+			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
+			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
+			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
+			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
+			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
+			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			</p>
+			
+			<p>
+			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
+			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
+			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
+			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
+			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
+			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			</p>
+			
+			<p>
+			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
+			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
+			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
+			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
+			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
+			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			</p>		
+		
+		</span>)
     }
 });
