b06a25bc59a9ac73997cd443b76caa4b697a7b87
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 12:42:52 -0800

    Update main.jsx

--- files/app/truefactors/documentPage/main.jsx
+++ files/app/truefactors/documentPage/main.jsx
@@ -29,7 +29,7 @@ React.createClass({
           var me = this;
           return  (
 		<div className="container">
-			<div className="row documentPageFrame mt-1 p-2">
+			<div className="row documentPageFrame mt-1 p-1">
 			{Root.commUI.show({
 					code: 'documentPage', 
 					parent : me, 
