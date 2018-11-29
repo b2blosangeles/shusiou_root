1d0ba7844fb671dbb48a9b0ba5ab7ba97de171ea
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 11:42:03 -0800

    Update contact.jsx

--- files/app/truefactors/documentPage/contact.jsx
+++ files/app/truefactors/documentPage/contact.jsx
@@ -1,8 +1,9 @@
 var _contact = React.createClass({
     getInitialState: function() {
-		var me = this;
+	var me = this;
+	return {};
     },
     render: function() {
-           return  (<span>Contact Us</span>)
+	return  (<span>Contact Us</span>)
     }
 });
