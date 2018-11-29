1b75682f8d058ba08d1dc7a6ce2f16139226b2d0
b2blosangeles <b2blosangeles@gmail.com>
2018-11-28 18:21:17 -0800

    Update _commLib.jsx

--- files/app/truefactors/_commLib.jsx
+++ files/app/truefactors/_commLib.jsx
@@ -15,7 +15,7 @@ var _commLib = function () {
 	return r;
     }
     this.setSpinner = function(parent, s) {
-	parent.setState({spinnner : (s) ? true : null})
+	parent.setState({spinner : (s) ? true : null})
     }
    /*========== Need review ====*/
     this.getAuth = function() {
