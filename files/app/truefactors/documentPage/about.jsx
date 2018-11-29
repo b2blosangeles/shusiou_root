8859f605af4e0a294bb20a7e9bd0368e8f70ecb5
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 14:10:15 -0800

    Update about.jsx

--- files/app/truefactors/documentPage/about.jsx
+++ files/app/truefactors/documentPage/about.jsx
@@ -4,6 +4,17 @@ var _about = React.createClass({
 	return {}
     },
     render: function() {
-	return  (<span>About Us</span>)
+	return  (<span>
+			<h3>About Us<h3/>
+			<p>
+			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
+			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
+			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
+			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
+			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
+			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			</p>
+		
+		</span>)
     }
 });
