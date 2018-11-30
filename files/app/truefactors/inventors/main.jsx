3fc76fe1f371143a2536b2f5dcec4b247b98c8f4
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 20:31:18 -0800

    Update main.jsx

--- files/app/truefactors/inventors/main.jsx
+++ files/app/truefactors/inventors/main.jsx
@@ -72,7 +72,7 @@ React.createClass({
 				</div>
 				<div className="row mt-2">
 					{Root.commUI.show({code: 'spinner', parent: me})}
-					{Root.commUI.show({ data: me.popupBody(),code: 'popup', parent: me})}
+					{Root.commUI.show({'popup',  data: me.popupBody(),code: parent: me})}
 					
 					{me.list.map(function(m) {
 					return (<div className="col-sm-3 p-1">
