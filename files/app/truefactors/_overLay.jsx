7cb2662a1dab996f6cb9550b7eb1f1623015b6b7
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 21:45:29 -0800

    Update _overLay.jsx

--- files/app/truefactors/_overLay.jsx
+++ files/app/truefactors/_overLay.jsx
@@ -1,12 +1,12 @@
 try {
 	var _overLay = React.createClass({
-	    render : function () { 
-		var me = this;
-		return (
-			<span>_overLay</span>                   
-		)
-	    }
-	})
+		render: function() {
+
+			var me = this;
+			return (
+				<span>_overLay</span>                   
+			)}
+		})
 } catch (err) {
 	console.log(err.message);
 }
