c171cc75ab9dc7c15564bf7be2e1f42545379ec2
b2blosangeles <b2blosangeles@gmail.com>
2018-11-30 08:48:12 -0800

    Update _asyncModule.jsx

--- files/app/truefactors/_asyncModule.jsx
+++ files/app/truefactors/_asyncModule.jsx
@@ -80,7 +80,7 @@ try {
 							 data={(me.props.data !== null) ? me.props.data : null}
 							 update = {(me.props.update) ? me.props.update : ''}
 							 />
-							{(!_overLay) ? '===ppppp====' : '(<_overLay />)'}
+							{(!_overLay) ? '===ppppp====' : typeof _overLay}
 						</span>)
 							 
 				} catch (err) {
