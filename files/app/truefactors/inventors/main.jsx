bc5480bf3ffab5e490b8a050889ec0cf12af875b
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 20:40:43 -0800

    Update main.jsx

--- files/app/truefactors/inventors/main.jsx
+++ files/app/truefactors/inventors/main.jsx
@@ -51,6 +51,8 @@ React.createClass({
 			<button type="button" className="btn btn-info" onClick={me.loadData.bind(me)}>Load Data</button>
 			&nbsp;
 			<button type="button" className="btn btn-info" onClick={me.popup.bind(me)}>Popup Window</button>
+			&nbsp;
+			<button type="button" className="btn btn-info" onClick={Root.pointObj.test.bind(me)}>niu</button>
 		</span>
 		)
 		return  (
