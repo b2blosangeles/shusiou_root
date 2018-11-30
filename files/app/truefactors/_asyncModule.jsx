9c83351286b5c4bac3c4ca089319c9fcee3bf974
b2blosangeles <b2blosangeles@gmail.com>
2018-11-29 20:23:41 -0800

    Update _asyncModule.jsx

--- files/app/truefactors/_asyncModule.jsx
+++ files/app/truefactors/_asyncModule.jsx
@@ -69,7 +69,7 @@ try {
 						Root); 
 					}
 					return  (<_asyncOBJ parent={(me.props.parent) ? me.props.parent : me}
-							 data={(me.props.data) ? me.props.data : {}}
+							 data={(me.props.data !== null) ? me.props.data : {}}
 							 update = {(me.props.update) ? me.props.update : ''}
 							 />)
 							 
