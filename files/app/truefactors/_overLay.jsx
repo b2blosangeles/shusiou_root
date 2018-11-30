3b371374f702760cf76c061bbe7a40117da87556
b2blosangeles <b2blosangeles@gmail.com>
2018-11-30 12:37:15 -0800

    Update _overLay.jsx

--- files/app/truefactors/_overLay.jsx
+++ files/app/truefactors/_overLay.jsx
@@ -1,45 +1,40 @@
-try {
+React.createClass({
+	getInitialState: function() {
+		var me = this;
+		me.loading = [];
 
-	var _classOverLay = React.createClass({
-		getInitialState: function() {
-			var me = this;
-			me.loading = [];
-			
-			return {RootReady : 'A'};
-		},
-		componentDidMount : function() {
-			var me = this;
-			window.__rootOverLay = me;
-			console.log('componentDidMount--2->');
-			me.setState({RootReady : 'B'})
-		},
-		componentDidUpdate : function() {
-			var me = this;
-			console.log('componentDidUpdate--->');
-			//if (!Root) {
-				//if ((Root) && (!Root.overLay)) {
-				
-			//	console.log('Parking overLay');
-				//Root.overLay = me;
-			//	me.setState({RootReady : true})
-			//}
-		},
-		test : function() {
-			if (typeof Root === 'undefined' ) alert('test 1');
-			else  alert('test 2');
-			// || !Root.overLay
-		},
-		render: function() {
-			var me = this;
-			return ((typeof Root === 'undefined' ) ? (<span>No ROOT--{me.state.RootReady}</span>):
-				(<span>
-					Overlay ready ===
-					{Root.commUI.show({code: 'spinner', parent: me})}
-					{Root.commUI.show({code: 'popup',  data: 'me.popupBody()', parent: me})}
-				</span>)                   
-			)}
-	})
-} catch (err) {
-	console.log(err.message);
-}
+		return {RootReady : 'A'};
+	},
+	componentDidMount : function() {
+		var me = this;
+		window.__rootOverLay = me;
+		console.log('componentDidMount--2->');
+		me.setState({RootReady : 'B'})
+	},
+	componentDidUpdate : function() {
+		var me = this;
+		console.log('componentDidUpdate--->');
+		//if (!Root) {
+			//if ((Root) && (!Root.overLay)) {
+
+		//	console.log('Parking overLay');
+			//Root.overLay = me;
+		//	me.setState({RootReady : true})
+		//}
+	},
+	test : function() {
+		if (typeof Root === 'undefined' ) alert('test 1');
+		else  alert('test 2');
+		// || !Root.overLay
+	},
+	render: function() {
+		var me = this;
+		return ((typeof Root === 'undefined' ) ? (<span>No ROOT--{me.state.RootReady}</span>):
+			(<span>
+				Overlay ready ===
+				{Root.commUI.show({code: 'spinner', parent: me})}
+				{Root.commUI.show({code: 'popup',  data: 'me.popupBody()', parent: me})}
+			</span>)                   
+		)}
+})
 
