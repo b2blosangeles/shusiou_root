2158cf33bca4114ec1db596d084611a92aceb419
b2blosangeles <b2blosangeles@gmail.com>
2018-11-30 19:29:55 -0800

    Update loadData.js

--- files/app/truefactors/inventors/loadData.js
+++ files/app/truefactors/inventors/loadData.js
@@ -7,7 +7,7 @@ var loadVideos = function(me, Root) {
 			function() {
 				me.list = Root.lib.getNumberList(18);
 				Root.overLay.spinOff(spin_id);
-				console.log(spin_id);
+			//	console.log(spin_id);
 				me.setState({updated : new Date().getTime()});
 			},1000
 		)
