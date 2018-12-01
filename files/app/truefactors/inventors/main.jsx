2d0c39d532f2f2eb3d9d99b344c8e5883d733f01
b2blosangeles <b2blosangeles@gmail.com>
2018-11-30 17:57:05 -0800

    Update main.jsx

--- files/app/truefactors/inventors/main.jsx
+++ files/app/truefactors/inventors/main.jsx
@@ -89,7 +89,7 @@ React.createClass({
 	},
 	showBody : function() {
 		var me = this;
-		if (me.props.parent.state.menuOption === 'myVideos') {
+		if (me.props.parent.state.menuOption === 'myVideos' || me.props.parent.state.menuOption === '') {
 			return me.myVideos();
 		} else {
 			return (<span>No update</span>)
