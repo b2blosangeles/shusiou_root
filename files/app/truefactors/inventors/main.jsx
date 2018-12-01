f05e9206a252c2ddbfabe81992d977d3715d99da
b2blosangeles <b2blosangeles@gmail.com>
2018-11-30 19:31:54 -0800

    Update main.jsx

--- files/app/truefactors/inventors/main.jsx
+++ files/app/truefactors/inventors/main.jsx
@@ -17,19 +17,7 @@ React.createClass({
 	},
 	loadData : function() {
 		var me = this;
-		loadVideos(me, Root);
-		/*
-		var spin_id = Root.overLay.spinOn();
-		me.list = [];
-		me.setState({updated : new Date().getTime()});
-		setTimeout(
-			function() {
-				me.list = Root.lib.getNumberList(18);
-				Root.overLay.spinOff(spin_id);
-				console.log(spin_id);
-				me.setState({updated : new Date().getTime()});
-			},1000
-		)*/	
+		loadVideos(me);
 	},
 	popupBody : function() {
 		var me = this;
