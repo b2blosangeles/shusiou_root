32c686df326092d1e48459c783087751ee2b68e9
b2blosangeles <b2blosangeles@gmail.com>
2018-12-01 22:38:15 -0800

    Update data.jsx

--- files/app/truefactors/inventors/data.jsx
+++ files/app/truefactors/inventors/data.jsx
@@ -1,14 +1,14 @@
 var _compData = function(me, Root) {
 	this.loadVideos = function() {
-		//var spin_id = Root.overLay.spinOn();
-		Root.lib.setSpinAnchor(me, 'AA', true);
+		var spin_id = Root.overLay.spinOn();
+		//Root.lib.setSpinAnchor(me, 'AA', true);
 		me.list = [];
 		me.setState({updated : new Date().getTime()});
 		setTimeout(
 			function() {
 				me.list = Root.lib.getNumberList(18);
-				Root.lib.setSpinAnchor(me, 'AA', false)
-				// Root.overLay.spinOff(spin_id);
+				// Root.lib.setSpinAnchor(me, 'AA', false)
+				Root.overLay.spinOff(spin_id);
 				me.setState({updated : new Date().getTime()});
 			},1000
 		)
