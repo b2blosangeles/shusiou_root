cd06d3224f2c91af2496abf4c830d4a9c32dc1ab
b2blosangeles <b2blosangeles@gmail.com>
2018-12-01 19:14:07 -0800

    Update _commLib.jsx

--- files/app/truefactors/_commLib.jsx
+++ files/app/truefactors/_commLib.jsx
@@ -22,7 +22,7 @@ var _commLib = function () {
 			return {};
 		},	
 		render: function() {
-			var me = this;
+			//var me = this;
 			return
 			(!me.state.spinAnchor || !me.state.spinAnchor[code]) ? (<span>nothing</span>) :
 			(<span><span className="section_spin_cover"></span><span className="section_spin_page">
