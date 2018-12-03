c91453e5cbe54dab3778569949d6d921b54db36d
b2blosangeles <b2blosangeles@gmail.com>
2018-12-03 10:43:27 -0800

    Update view.jsx

--- files/app/truefactors/inventors/view.jsx
+++ files/app/truefactors/inventors/view.jsx
@@ -1,5 +1,15 @@
 var _compView = function(me, Root) {
-	this.test = function() {
+	this.showVideoMenu = function() {
+		var popupSetting = {
+			type : 'light', 
+			// style : {'min-height' : '12em', 'border' : '3px solid #666 !important'},
+			closeIcon : true,
+			data : (<span>
+				<button type="button" className="btn btn-warning"  
+					onClick={Root.overLay.closePopup.bind(me)}>close Popup Window</button>
+				</span>
+				)
+		};
 		return (<span>	
 		<button type="button" className="btn btn-info" onClick={me.compData.loadVideos.bind(me)}>Load Data</button>
 		&nbsp;
