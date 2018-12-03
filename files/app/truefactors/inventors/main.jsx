eca4e532967e0b89b5c2f3c46d8378e8d49cc66e
b2blosangeles <b2blosangeles@gmail.com>
2018-12-03 10:59:10 -0800

    Update main.jsx

--- files/app/truefactors/inventors/main.jsx
+++ files/app/truefactors/inventors/main.jsx
@@ -27,17 +27,16 @@ React.createClass({
 	},
         render: function() {
 		var me = this;
-		return ({Root.commUI.show({
-					code: 'infoBox', 
-					parent : me, 
-					data : me.showBody(), 
-					setting : {
-						type : 'light',
-						noshadow : true,
-						style : {'min-height' : '40em'},
-						class : 'documentPageBody p-3'
-					  }
-				})}
-			)
+		return Root.commUI.show({
+				code: 'infoBox', 
+				parent : me, 
+				data : me.showBody(), 
+				setting : {
+					type : 'light',
+					noshadow : true,
+					style : {'min-height' : '40em'},
+					class : 'documentPageBody p-3'
+				  }
+			})
         }
 })
