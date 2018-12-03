6e7ff7c84af0afe26260417e1370eae40e6fbe72
b2blosangeles <b2blosangeles@gmail.com>
2018-12-03 11:58:59 -0800

    Update main.jsx

--- files/app/truefactors/root/main.jsx
+++ files/app/truefactors/root/main.jsx
@@ -59,9 +59,9 @@ React.createClass({
 								'https://dev.shusiou.win/app/truefactors/investors/data.jsx',
 								'https://dev.shusiou.win/app/truefactors/investors/view.jsx'	
 							],
-							main : 'https://dev.shusiou.win/app/truefactors/investors/main.jsx'
+							controller : 'https://dev.shusiou.win/app/truefactors/investors/main.jsx'
 						}, 
-						master: '//master1_dev.shusiou.win/api/JSXhub.api'
+						master: '//master1_dev.shusiou.win/api/DVCHub.api'
 					},
 				data : '',
 				parent : me
