028a61247c4cb8f172114e913e07241b5b07d566
b2blosangeles <b2blosangeles@gmail.com>
2018-12-03 10:35:17 -0800

    Update view.jsx

--- files/app/truefactors/inventors/view.jsx
+++ files/app/truefactors/inventors/view.jsx
@@ -1,5 +1,9 @@
 var _compView = function(me, Root) {
 	this.test = function() {
-		return (<span>TT</span>)
+		return (<span>	
+		<button type="button" className="btn btn-info" onClick={me.compData.loadVideos.bind(me)}>Load Data</button>
+		&nbsp;
+		<button type="button" className="btn btn-info" onClick={Root.overLay.popup.bind(me, popupSetting)}>Popup Window</button>
+		</span>)
 	}
 };
