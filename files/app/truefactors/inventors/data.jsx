1d47431987daad20108b088c3aec055b135f0d5c
b2blosangeles <b2blosangeles@gmail.com>
2018-12-02 19:50:03 -0800

    Update data.jsx

--- files/app/truefactors/inventors/data.jsx
+++ files/app/truefactors/inventors/data.jsx
@@ -10,7 +10,7 @@ var _compData = function(me, Root) {
 				// Root.lib.setSpinAnchor(me, 'AA', false)
 				Root.overLay.spinOff(spin_id);
 				me.setState({updated : new Date().getTime()});
-			},4000
+			},1000
 		)
 	},
 	this.showImage = function(i) {
