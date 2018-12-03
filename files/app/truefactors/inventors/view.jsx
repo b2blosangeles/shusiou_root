43b2e941ca1b3c8dac700b110ab2d60768d529e5
b2blosangeles <b2blosangeles@gmail.com>
2018-12-03 10:50:55 -0800

    Update view.jsx

--- files/app/truefactors/inventors/view.jsx
+++ files/app/truefactors/inventors/view.jsx
@@ -15,5 +15,43 @@ var _compView = function(me, Root) {
 		&nbsp;
 		<button type="button" className="btn btn-info" onClick={Root.overLay.popup.bind(me, popupSetting)}>Popup Window</button>
 		</span>)
+	},
+	this.showVideos : function () {
+		return  (
+			<div className="container">
+				{Root.lib.spinAnchor(me, 'AA')}
+				<div className="row ">
+					<div className="col-sm-12 p-1">	
+					{Root.commUI.show({
+							code: 'infoBox', 
+							parent : me, 
+							data : me.compView.showVideoMenu(), 
+							setting : {
+								class : ' alert-secondary text-right ',
+								noshadow :  true,
+								rounded : true,
+								style : {'min-height' : '2em'}
+							}
+						})}
+					</div>
+				</div>
+				<div className="row mt-2">				
+					{me.list.map(function(m) {
+					return (<div className="col-sm-3 p-1">
+						{Root.commUI.show({
+							code: 'infoBox', 
+							parent : me, 
+							data : me.compData.showImage(m), 
+							setting : {
+								noshadow :false,
+								type: 'light',
+								style : {'min-height' : '9em'}
+							}
+						})}
+						</div>)
+					})}							
+				</div>
+			</div>)	
 	}
+	
 };
