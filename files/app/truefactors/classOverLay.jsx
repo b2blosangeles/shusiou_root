c5524344d9559ec1830c239881b853ccc1469b03
b2blosangeles <b2blosangeles@gmail.com>
2018-12-04 09:27:38 -0800

    Update classOverLay.jsx

--- files/app/truefactors/classOverLay.jsx
+++ files/app/truefactors/classOverLay.jsx
@@ -96,7 +96,7 @@ React.createClass({
 		me.setState({_popup : true})
 		
 		setTimeout(function() { 
-			$('.overlay_popup_page').hide().show( "slide", {direction: "up" }, 2000 )
+			$('.overlay_popup_page').hide().show( "slide", {direction: "right" }, 2000 )
 		//	$('.overlay_popup_page').hide().slideDown(600);
 		});
 	},
