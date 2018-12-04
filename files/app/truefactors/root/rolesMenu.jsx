ea61ac125c22258c9547ef37a02e6c2dbd85e2cc
b2blosangeles <b2blosangeles@gmail.com>
2018-12-04 15:57:31 -0800

    Update rolesMenu.jsx

--- files/app/truefactors/root/rolesMenu.jsx
+++ files/app/truefactors/root/rolesMenu.jsx
@@ -32,13 +32,7 @@ var _rolesMenu = React.createClass({
 								((me.props.parent.state.role === idx) ? 
 									  'role-checked' : 'role-unchecked')}
 								onClick={me.clickMenu.bind(me, idx)}>
-								<b>{me.roles[idx].caption}</b></button>
-							  {/* <button className={'btn btn-large rounded ' + 
-								((me.props.parent.state.role === idx) ? 
-									  'role-checked' : 'role-unchecked')}
-								onClick={me.pickMenu.bind(me, idx)}>
-								<b>{me.roles[idx].caption}</b></button>*/}
-							})}
+								<b>{me.roles[idx].caption}</b></button>})}
 						</div>
 					</div>
 					</div>
