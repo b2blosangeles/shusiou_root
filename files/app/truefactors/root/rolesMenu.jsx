b043a0f81fd2aca1d8477ed6e8dfc624b6713209
b2blosangeles <b2blosangeles@gmail.com>
2018-12-04 11:29:48 -0800

    Update rolesMenu.jsx

--- files/app/truefactors/root/rolesMenu.jsx
+++ files/app/truefactors/root/rolesMenu.jsx
@@ -16,6 +16,7 @@ var _rolesMenu = React.createClass({
 		console.log('=== niu 3 ===');
 	},
 	animation : function(sobj, tobj) {
+		 sobj.html('===niu A===');
 		 tobj.html('===niu===');
 		return true;
 		console.log(sobj);
