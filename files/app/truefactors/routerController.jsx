bd7a1d6ee737e87df081c28d0afa27c625c468d0
b2blosangeles <b2blosangeles@gmail.com>
2018-12-05 12:30:34 -0800

    Update routerController.jsx

--- files/app/truefactors/routerController.jsx
+++ files/app/truefactors/routerController.jsx
@@ -2,8 +2,12 @@ React.createClass({
 	render: function() {
 		var me = this;
 		return (<ReactRouter.Router history={hashHistory}>
+				<IndexRoute env={me}/>
+				{/*me.routeMatrix()*/}
+			</ReactRouter.Router>)  
+		/* return (<ReactRouter.Router history={hashHistory}>
 				<IndexRoute env={me} component={Ad}/>
 				{me.routeMatrix()}
-			</ReactRouter.Router>)                   
+			</ReactRouter.Router>) */
 	}
 })
