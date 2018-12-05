3e7352e4dd1908f7a93bdb2eab9645904ac5da21
b2blosangeles <b2blosangeles@gmail.com>
2018-12-04 18:04:46 -0800

    Update data.jsx

--- files/app/truefactors/inventors/data.jsx
+++ files/app/truefactors/inventors/data.jsx
@@ -6,7 +6,7 @@ var _compData = function(me, Root) {
 		me.setState({updated : new Date().getTime()});
 		setTimeout(
 			function() {
-				me.list = Root.lib.getNumberList(18);
+				me.list = Root.lib.getNumberList(11);
 				Root.lib.setSpinAnchor(me, 'AA', false)
 			//	Root.overLay.spinOff(spin_id);
 				me.setState({updated : new Date().getTime()});
