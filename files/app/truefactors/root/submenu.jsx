a4c48a40f40332f1e0a5d40e31a0e4fbe53436ed
b2blosangeles <b2blosangeles@gmail.com>
2018-12-06 15:10:19 -0800

    Update submenu.jsx

--- files/app/truefactors/root/submenu.jsx
+++ files/app/truefactors/root/submenu.jsx
@@ -22,7 +22,7 @@ var _subMenu = React.createClass({
 	  	? [] : Root.global.menuTree[me.props.parent.state.role];
 	  var captions = Root.global.menuItems
 	  
-	  var publicMenu = (!menu.length) ? [] : Root.global.menuTree._publicMenu;
+	  var publicMenu = (!menu.length) ? Root.global.menuTree._publicMenu : [];
           return  (
             <div className="secondMenu container-fluid" style={{'height' : '2.5em', color:'#fff'}}>
 		<div className="container">
