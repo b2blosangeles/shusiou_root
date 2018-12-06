f82248be3393bba02fa933a0becc1f6f237f2dea
b2blosangeles <b2blosangeles@gmail.com>
2018-12-06 15:04:38 -0800

    Update submenu.jsx

--- files/app/truefactors/root/submenu.jsx
+++ files/app/truefactors/root/submenu.jsx
@@ -22,6 +22,7 @@ var _subMenu = React.createClass({
 	  	? [] : Root.global.menuTree[me.props.parent.state.role];
 	  var captions = Root.global.menuItems
 	  
+	  var publicMenu = (!menu.length) ? [] : Root.global.menuTree._publicMenu;
           return  (
             <div className="secondMenu container-fluid" style={{'height' : '2.5em', color:'#fff'}}>
 		<div className="container">
@@ -45,6 +46,24 @@ var _subMenu = React.createClass({
 											{captions[m]}</span></a>
 									</li></span>)
 							})}
+						
+					{publicMenu.map(function(m){ 	
+						return (me.props.parent.state.menuOption !== m) ?
+								(<span>
+									<li className="nav-item">
+										<a className="nav-link active" 
+										onClick={me.clickOption.bind(me, m)}
+										href="JavaScript: void(0)">{captions[m]}</a>
+									</li></span>)
+								:
+								(<span>
+									<li className="nav-item">
+										<a className="nav-link disabled" 
+										href="JavaScript: void(0)">
+										<span style={{color:'yellow'}}>
+											{captions[m]}</span></a>
+									</li></span>)
+							})}						
 					</ul>					
 				</div>
 			</div>
