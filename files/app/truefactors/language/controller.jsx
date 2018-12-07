c0ff431c3f3ee49415b5d1fcc09af4d83eaef942
b2blosangeles <b2blosangeles@gmail.com>
2018-12-07 06:56:50 -0800

    Create controller.jsx

---
+++ files/app/truefactors/language/controller.jsx
@@ -0,0 +1,42 @@
+React.createClass({
+        getInitialState: function() {
+		var me = this;
+		me.list = [];
+		me.compData = new _compData(me, Root);
+		me.compView = new _compView(me, Root);
+          	return {}
+        },
+	componentDidMount : function() {
+		var me = this;
+		
+		if (me.props.parent.state.menuOption === 'myVideos' || !me.props.parent.state.menuOption) {
+			me.compData.loadVideos();
+			// me.loadData();
+		}
+	},
+	componentWillUnmount : function() {
+		var me = this;
+	},
+	showBody : function() {
+		var me = this;
+		if (me.props.parent.state.menuOption === 'myVideos' || !me.props.parent.state.menuOption) {
+			return me.compView.showVideos();
+		} else {
+			return (<span>No document</span>)
+		}	
+	},
+        render: function() {
+		var me = this;
+		return Root.commUI.show({
+				code: 'infoBox', 
+				parent : me, 
+				data : me.showBody(), 
+				setting : {
+					type : 'light',
+					noshadow : true,
+					style : {'min-height' : '40em'},
+					class : 'documentPageBody p-3'
+				  }
+			})
+        }
+})
