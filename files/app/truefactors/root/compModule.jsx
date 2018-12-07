336ae14ba53cbb0156fcfd2088f37b0c012eec52
b2blosangeles <b2blosangeles@gmail.com>
2018-12-07 09:06:16 -0800

    Update compModule.jsx

--- files/app/truefactors/root/compModule.jsx
+++ files/app/truefactors/root/compModule.jsx
@@ -1,3 +1,17 @@
 var _compModule = function(me, Root) {
-
+    this.loadInventors = function() {
+				return Root.lib.asyncModule({
+					setting:{	extend: {
+								includes : [
+									'https://dev.shusiou.win/app/truefactors/inventors/data.jsx',
+									'https://dev.shusiou.win/app/truefactors/inventors/view.jsx'	
+								],
+								main : 'https://dev.shusiou.win/app/truefactors/inventors/main.jsx'
+							}, 
+							master: '//master1_dev.shusiou.win/api/JSXhub.api'
+						},
+					data : '',
+					parent : me
+				})
+    }
 };
