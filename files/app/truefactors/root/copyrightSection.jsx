7c49d1ac6bd721a765a209f3357029c0a4bbef05
b2blosangeles <b2blosangeles@gmail.com>
2018-12-06 20:11:03 -0800

    Update copyrightSection.jsx

--- files/app/truefactors/root/copyrightSection.jsx
+++ files/app/truefactors/root/copyrightSection.jsx
@@ -11,26 +11,26 @@ var _copyrightSection = React.createClass({
 			<div className="container-fluid">
 			<div className="row">
 				<div className="col-sm-3 text-left">
-					<a href={'#' + role + '/'} onClick={me.animationTransfer}>English</a>
+					<a href={'#' + role + '/'} onClick={me.props.parent.animationTransfer}>English</a>
 					&nbsp;&nbsp;|&nbsp;&nbsp;							
-					<a href={'#' + role + '/privacy'} onClick={me.animationTransfer}>Chinese</a>					
+					<a href={'#' + role + '/privacy'} onClick={me.props.parent.animationTransfer}>Chinese</a>					
 				</div>
 				<div className="col-sm-9 text-right">
 					<span className="pull-right">
 					&#169; {new Date().getFullYear()} Plato Plan
 					&nbsp;&nbsp;&nbsp;&nbsp;
-					<a href={'#' + role + '/'} onClick={me.animationTransfer}>Home</a>
+					<a href={'#' + role + '/'} onClick={me.props.parent.animationTransfer}>Home</a>
 					&nbsp;&nbsp;|&nbsp;&nbsp;							
-					<a href={'#' + role + '/privacy'} onClick={me.animationTransfer}>Privacy</a>
+					<a href={'#' + role + '/privacy'} onClick={me.props.parent.animationTransfer}>Privacy</a>
 					&nbsp;&nbsp;|&nbsp;&nbsp;
-					<a href={'#' + role + '/terms'} onClick={me.animationTransfer}>Terms</a>
+					<a href={'#' + role + '/terms'} onClick={me.props.parent.animationTransfer}>Terms</a>
 					</span>
 
 					<span className="pull-left">
 						{Root.global.menuTree.common.map(function(m) {
 						return(<span>
 						  &nbsp;&nbsp;|&nbsp;&nbsp;
-						  <a href={'#' + role + '/' + m}  onClick={me.animationTransfer}>
+						  <a href={'#' + role + '/' + m}  onClick={me.props.parent.animationTransfer}>
 							  {Root.global.menuItems[m]}</a>
 						</span>)
 						})}
