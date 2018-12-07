fefb5a05fd119a5c9dd03517a595a29d04d7d9d2
b2blosangeles <b2blosangeles@gmail.com>
2018-12-07 09:55:01 -0800

    Update controller.jsx

--- files/app/truefactors/language/controller.jsx
+++ files/app/truefactors/language/controller.jsx
@@ -3,8 +3,22 @@ React.createClass({
 		var me = this;
           	return {}
         },
+	showBody : function() {
+		var me = this;
+		return (<span>Language</span>)	
+	},
         render: function() {
 		var me = this;
-		return (<span>Language</span>)
+		return Root.commUI.show({
+				code: 'infoBox', 
+				parent : me, 
+				data : me.showBody(), 
+				setting : {
+					type : 'light',
+					noshadow : true,
+					style : {'min-height' : '40em'},
+					class : 'documentPageBody p-3'
+				  }
+			})
         }
 })
