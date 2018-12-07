7cbc0dfd94043612565f881e0f63cd63b925d695
b2blosangeles <b2blosangeles@gmail.com>
2018-12-07 09:07:13 -0800

    Update controller.jsx

--- files/app/truefactors/root/controller.jsx
+++ files/app/truefactors/root/controller.jsx
@@ -52,21 +52,7 @@ React.createClass({
 		    Root.global.menuTree[me.state.role].indexOf(me.state.menuOption) !== -1 && 
 		    Root.global.menuTree._publicMenu.indexOf(me.state.menuOption) === -1 
 		   ) {
-			if (me.state.role === 'inventor') {
-				return Root.lib.asyncModule({
-					setting:{	extend: {
-								includes : [
-									'https://dev.shusiou.win/app/truefactors/inventors/data.jsx',
-									'https://dev.shusiou.win/app/truefactors/inventors/view.jsx'	
-								],
-								main : 'https://dev.shusiou.win/app/truefactors/inventors/main.jsx'
-							}, 
-							master: '//master1_dev.shusiou.win/api/JSXhub.api'
-						},
-					data : '',
-					parent : me
-				})
-			}		
+			if (me.state.role === 'inventor')  return me.compModule.loadInventors()	
 		
 			if (me.state.role === 'investor') {
 				return Root.lib.asyncModule({
