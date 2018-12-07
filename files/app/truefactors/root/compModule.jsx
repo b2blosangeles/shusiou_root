c7ca46ff7aa13189051d42d66021086c48968593
b2blosangeles <b2blosangeles@gmail.com>
2018-12-07 09:09:18 -0800

    Update compModule.jsx

--- files/app/truefactors/root/compModule.jsx
+++ files/app/truefactors/root/compModule.jsx
@@ -1,17 +1,36 @@
 var _compModule = function(me, Root) {
-    this.loadInventors = function() {
-				return Root.lib.asyncModule({
-					setting:{	extend: {
-								includes : [
-									'https://dev.shusiou.win/app/truefactors/inventors/data.jsx',
-									'https://dev.shusiou.win/app/truefactors/inventors/view.jsx'	
-								],
-								main : 'https://dev.shusiou.win/app/truefactors/inventors/main.jsx'
-							}, 
-							master: '//master1_dev.shusiou.win/api/JSXhub.api'
-						},
-					data : '',
-					parent : me
-				})
+	
+    this.loadInventor = function() {
+	return Root.lib.asyncModule({
+		setting:{	extend: {
+					includes : [
+						'https://dev.shusiou.win/app/truefactors/inventors/data.jsx',
+						'https://dev.shusiou.win/app/truefactors/inventors/view.jsx'	
+					],
+					main : 'https://dev.shusiou.win/app/truefactors/inventors/main.jsx'
+				}, 
+				master: '//master1_dev.shusiou.win/api/JSXhub.api'
+			},
+		data : '',
+		parent : me
+	})
+    }
+    
+    this.loadInvestor = function() {
+	return Root.lib.asyncModule({
+		setting:{	extend: {
+					contents : {
+					},					
+					includes : [
+						'https://dev.shusiou.win/app/truefactors/investors/data.jsx',
+						'https://dev.shusiou.win/app/truefactors/investors/view.jsx'	
+					],
+					controller : 'https://dev.shusiou.win/app/truefactors/investors/controller.jsx'
+				}, 
+				master: '//master1_dev.shusiou.win/api/DVCHub.api'
+			},
+		data : '',
+		parent : me
+	})
     }
 };
