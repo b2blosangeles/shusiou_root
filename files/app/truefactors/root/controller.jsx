99059c2685fe3b3fa0fbce2764a389e9722bf559
b2blosangeles <b2blosangeles@gmail.com>
2018-12-08 08:24:54 -0800

    Update controller.jsx

--- files/app/truefactors/root/controller.jsx
+++ files/app/truefactors/root/controller.jsx
@@ -97,7 +97,7 @@ React.createClass({
         render: function() {
           var me = this;
           return  (
-		<span>
+		<span> ==--== {new Date().getTime()}
 			<_routerControl routeRule={me.routeRule()} />
 			<_rolesMenu parent={me}/>
 			<_subMenu parent={me}/>
