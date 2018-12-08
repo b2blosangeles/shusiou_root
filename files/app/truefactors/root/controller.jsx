0a6dcce73639ddeefa843a566ccb9df5332401f5
b2blosangeles <b2blosangeles@gmail.com>
2018-12-08 01:16:19 -0800

    Update controller.jsx

--- files/app/truefactors/root/controller.jsx
+++ files/app/truefactors/root/controller.jsx
@@ -103,9 +103,8 @@ React.createClass({
 				var v = {};
 				if (path_a[0])  v.role  = path_a[0];
 				
-				if (path_a[1]) v.menuOption = path_a[1];
 				// else v.menuOption = '';
-				
+				if (path_a[1]) v.menuOption = path_a[1];
 				else {
 					if (!path_a[0]) {
 						var def_path = '';
