63cdf498b657e784d4855fa6caf1ab90e1be3d7b
b2blosangeles <b2blosangeles@gmail.com>
2018-12-08 07:02:36 -0800

    Update controller.jsx

--- files/app/truefactors/language/controller.jsx
+++ files/app/truefactors/language/controller.jsx
@@ -11,7 +11,7 @@ React.createClass({
 				{list.map(function(m) {
 					<div className="col-sm-1 p-1">{Root.global.langs[m]}==</div>
 				}
-			</row>
+			<div>
 		</div>)	
 	},
         render: function() {
