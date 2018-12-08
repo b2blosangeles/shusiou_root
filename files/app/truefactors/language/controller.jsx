2beb5577dde79ea4a3dc193cb94e4c0ab0de72c7
b2blosangeles <b2blosangeles@gmail.com>
2018-12-08 08:27:14 -0800

    Update controller.jsx

--- files/app/truefactors/language/controller.jsx
+++ files/app/truefactors/language/controller.jsx
@@ -13,14 +13,14 @@ React.createClass({
 		var list = Object.keys(Root.global.langs);
 		return (<div className="container">
 			<div className="row p-1 pt-4 m-2">	
-				<div className="col-sm-12 p-3 text-info">
+				<div className="col-sm-12 p-3 text-secondary">
 					Change language:	
 				</div>
 			</div>
 			<div className="row p-2 pt-3 m-2">	
 				{list.map(function(m) {
 					return (<a href={'#/language'}
-						className={'col-sm-2 m-2 mt-3 p-3 border border-info rounded text-center text-info ' + 
+						className={'col-sm-2 m-2 mt-3 p-3 border border-secondary rounded text-center text-secondary ' + 
 							((m == me.state.lang) ? 'role-checked' : 'role-unchecked') }
 						onClick={me.switchLang.bind(me, m)}	
 						>
