5b914273415ab896eff432eb966eed4631c3ac0f
b2blosangeles <b2blosangeles@gmail.com>
2018-12-08 10:25:24 -0800

    Update contact.jsx

--- files/app/truefactors/documentPage/contact.jsx
+++ files/app/truefactors/documentPage/contact.jsx
@@ -9,10 +9,10 @@ var _contact = React.createClass({
 				<div className="col-sm-12 p-5 text-center"><h2>Contact Us</h2></div>
 			</div>	
 			<div className="row">
-				<div className="col-sm-12 p-3 text-center"><h3>(800) xxx-xxxx</h3></div>
+				<div className="col-sm-12 p-3 text-center text-info"><h3>(800) xxx-xxxx</h3></div>
 			</div>	
 			<div className="row">
-				<div className="col-sm-12 p-3 text-center"><h3>customer.service@platoplan.com</h3></div>
+				<div className="col-sm-12 p-3 text-center text-info"><h3>customer.service@platoplan.com</h3></div>
 			</div>	
 		</div>)
     }
