a06632d0bf5fb4c70553e3dbddcecbf4185a36d8
b2blosangeles <b2blosangeles@gmail.com>
2018-12-08 08:23:07 -0800

    Update controller.jsx

--- files/app/truefactors/language/controller.jsx
+++ files/app/truefactors/language/controller.jsx
@@ -14,7 +14,7 @@ React.createClass({
 		return (<div className="container">
 			<div className="row p-1 pt-4 m-2">	
 				<div className="col-sm-12 p-3 text-info">
-					Change language: {new Date().getTime()}	
+					Change language:	
 				</div>
 			</div>
 			<div className="row p-2 pt-3 m-2">	
