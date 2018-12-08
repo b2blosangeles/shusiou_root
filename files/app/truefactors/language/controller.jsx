73468847d419c6dc5ae0ddf0899d83244753337a
b2blosangeles <b2blosangeles@gmail.com>
2018-12-08 07:39:17 -0800

    Update controller.jsx

--- files/app/truefactors/language/controller.jsx
+++ files/app/truefactors/language/controller.jsx
@@ -7,8 +7,8 @@ React.createClass({
 		var me = this;
 		var list = Object.keys(Root.global.langs);
 		return (<div className="container">
-			<div className="row p-1 pt-5">	
-				<div className="col-sm-12 p-2">
+			<div className="row p-1 pt-4">	
+				<div className="col-sm-12 p-3">
 					Change language:	
 				</div>
 			</div>
