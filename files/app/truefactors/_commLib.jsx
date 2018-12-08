46b3ae1e04bdce1bb0beb3099bdfa54d7d6ae4ec
b2blosangeles <b2blosangeles@gmail.com>
2018-12-07 16:35:46 -0800

    Update _commLib.jsx

--- files/app/truefactors/_commLib.jsx
+++ files/app/truefactors/_commLib.jsx
@@ -8,14 +8,6 @@ var _commLib = function () {
 		__asyncOId = (!__asyncOId || __asyncOId > 1000000) ? 1 : (__asyncOId + 1);
 		var _oId = __asyncOId + '.' + new Date().getTime();
 		return <_asyncModule plugin={cfg.setting} data={cfg.data} parent={cfg.parent} objId={_oId}/>
-	}
-	this.asyncModuleA = function(config) { return function() {
-			var me = this;
-			var cfg = config;
-			__asyncOId = (!__asyncOId || __asyncOId > 1000000) ? 1 : (__asyncOId + 1);
-			var _oId = __asyncOId + '.' + new Date().getTime();
-			return <_asyncModule plugin={cfg.setting} data={cfg.data} parent={cfg.parent} objId={_oId}/>
-		}
 	}		
 	this.getNumberList = function(n) {
 	var r = [];
