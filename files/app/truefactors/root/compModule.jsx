3e911bd6b73b61fa44ef433f05506277921c3a42
b2blosangeles <b2blosangeles@gmail.com>
2018-12-08 19:08:46 -0800

    Update compModule.jsx

--- files/app/truefactors/root/compModule.jsx
+++ files/app/truefactors/root/compModule.jsx
@@ -4,7 +4,7 @@ var _compModule = function(me, Root) {
 	return Root.lib.asyncModule({
 		setting:{	extend: {
 					contents : {
-						howToStartInvention : 'https://dev.shusiou.win/app/truefactors/contents/howToStartInvention.txt'
+						howToStartInvestment : 'https://dev.shusiou.win/app/truefactors/contents/howToStartInvestment.txt'
 					},
 					includes : [
 						'https://dev.shusiou.win/app/truefactors/inventors/data.jsx',
