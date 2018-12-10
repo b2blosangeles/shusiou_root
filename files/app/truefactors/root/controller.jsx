ada75c6e5707e5bb4dc1839f0e4aac86b4516686
b2blosangeles <b2blosangeles@gmail.com>
2018-12-10 14:44:42 -0800

    Update controller.jsx

--- files/app/truefactors/root/controller.jsx
+++ files/app/truefactors/root/controller.jsx
@@ -92,6 +92,7 @@ React.createClass({
           var me = this;
           return  (
 		<span>
+			  <hr/>{JSON.stringify(Root.auth)}<hr/>
 			<_routerControl routeRule={me.routeRule()} />
 			  {/*<_rolesMenu parent={me}/>*/}
 			<_subMenu parent={me}/>
