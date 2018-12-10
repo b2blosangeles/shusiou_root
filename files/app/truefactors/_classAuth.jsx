35022e82c4f0ba444f1f3bd337645a7dde074663
b2blosangeles <b2blosangeles@gmail.com>
2018-12-10 14:13:39 -0800

    Update _classAuth.jsx

--- files/app/truefactors/_classAuth.jsx
+++ files/app/truefactors/_classAuth.jsx
@@ -13,7 +13,7 @@ React.createClass({
 	},
 	showStatus : function() {
 		var me = this;
-		return new Date().getTime();
+		// return new Date().getTime();
 		return ((me.state._auth) ? 'On' : 'Off') + '--SS-->' + new Date().getTime()
 	},
 	showSwitch : function() {
