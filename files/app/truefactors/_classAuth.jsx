6fc47c3f0da28ed4a4013061d93d4275dbaa5151
b2blosangeles <b2blosangeles@gmail.com>
2018-12-10 18:24:00 -0800

    Update _classAuth.jsx

--- files/app/truefactors/_classAuth.jsx
+++ files/app/truefactors/_classAuth.jsx
@@ -23,7 +23,7 @@ React.createClass({
 	doAuth : function() {
 		var me = this;
 		console.log('===doAuth===');
-		me.setState({_auth : (me.state._auth) ? false : true});
+		// me.setState({_auth : (me.state._auth) ? false : true});
 	},	
 	render: function() {
 		var me = this;
