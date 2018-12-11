2d606a98e53bc298b05a7818757c1c310b49c364
b2blosangeles <b2blosangeles@gmail.com>
2018-12-10 17:50:13 -0800

    Update controller.jsx

--- files/app/truefactors/root/controller.jsx
+++ files/app/truefactors/root/controller.jsx
@@ -94,7 +94,7 @@ React.createClass({
 		<span>
 			<_routerControl routeRule={me.routeRule()} />
 			<_rolesMenu parent={me}/>
-			<_subMenu parent={me} AP={Root} />
+			<_subMenu parent={me}  />
 			{me.showPageFrame(me.showBody())}
 			<_copyrightSection parent={me}/>
 		</span>
