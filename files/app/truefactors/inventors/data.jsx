e301e74c412af3eaf570a6be53c4c634a084cbad
b2blosangeles <b2blosangeles@gmail.com>
2018-12-12 10:23:55 -0800

    Update data.jsx

--- files/app/truefactors/inventors/data.jsx
+++ files/app/truefactors/inventors/data.jsx
@@ -28,32 +28,8 @@ var _compData = function(me, Root) {
 				me.setState({updated : new Date().getTime()});
 			},1000
 		)
-	},		
-	this.showImage = function(i) {
-		var list = [
-			'/app/truefactors/resource/webp/mqdefault_6s.webp',
-			'/app/truefactors/resource/webp/100.webp',
-			'/app/truefactors/resource/webp/101.webp',
-			'/app/truefactors/resource/webp/102.webp',
-			'/app/truefactors/resource/webp/103.webp',
-			'/app/truefactors/resource/webp/104.webp',
-			'/app/truefactors/resource/webp/105.webp',
-			'/app/truefactors/resource/webp/106.webp',
-			'/app/truefactors/resource/webp/107.webp',
-			'/app/truefactors/resource/webp/108.webp',
-			'/app/truefactors/resource/webp/109.webp',
-			'/app/truefactors/resource/webp/110.webp',
-			'/app/truefactors/resource/webp/111.webp',
-			'/app/truefactors/resource/webp/112.webp',
-			'/app/truefactors/resource/webp/113.webp',
-			'/app/truefactors/resource/webp/114.webp',
-			'/app/truefactors/resource/webp/115.webp',
-			'/app/truefactors/resource/webp/116.webp',
-			'/app/truefactors/resource/webp/117.webp'
-		]
-		var item = list[Math.floor(Math.random()*list.length)];
-		return '<img class="w-100" src="' + ((list[i]) ? list[i] : item) + '"/>';
-	}
+	}		
+
 	
 };
 
