391f37bb0a1ab7db763acdbb597aec3ae7b6b6a8
b2blosangeles <b2blosangeles@gmail.com>
2018-12-12 10:31:59 -0800

    Update view.jsx

--- files/app/truefactors/inventors/view.jsx
+++ files/app/truefactors/inventors/view.jsx
@@ -41,7 +41,7 @@ var _compView = function(me, Root) {
 						{Root.commUI.show({
 							code: 'infoBox', 
 							parent : me, 
-							data : this.showImage(m), 
+							data : me.compView.showImage(m), 
 							setting : {
 								noshadow :false,
 								type: 'light',
@@ -78,7 +78,7 @@ var _compView = function(me, Root) {
 						{Root.commUI.show({
 							code: 'infoBox', 
 							parent : me, 
-							data : this.showImage(m), 
+							data : me.compView.showImage(m), 
 							setting : {
 								noshadow :false,
 								type: 'light',
