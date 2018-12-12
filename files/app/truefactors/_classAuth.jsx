7132aae02c16a7e6f7f708aed1ac19eaa574038e
b2blosangeles <b2blosangeles@gmail.com>
2018-12-12 07:23:47 -0800

    Update _classAuth.jsx

--- files/app/truefactors/_classAuth.jsx
+++ files/app/truefactors/_classAuth.jsx
@@ -39,12 +39,12 @@ React.createClass({
 					      </div>
 
 					      <div className="form-label-group">
-						<input type="password" id="inputPassword" className="form-control" placeholder="Password" required.>
+						<input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
 						<label for="inputPassword">Password</label>
 					      </div>
 
 					      <div className="custom-control custom-checkbox mb-3">
-						<input type="checkbox" className="custom-control-input" id="customCheck1">
+						<input type="checkbox" className="custom-control-input" id="customCheck1"/>
 						<label className="custom-control-label" for="customCheck1">Remember password</label>
 					      </div>
 					      <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
