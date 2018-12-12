26133668f27f508f2e54568d741d5ff04e9a85b4
b2blosangeles <b2blosangeles@gmail.com>
2018-12-12 15:25:53 -0800

    Create controller.jsx

---
+++ files/app/truefactors/registration/controller.jsx
@@ -0,0 +1,10 @@
+React.createClass({
+        render: function() {
+          var me = this;
+          return  (
+            <span>
+              registration page 
+            </span>
+          )
+        }
+})
