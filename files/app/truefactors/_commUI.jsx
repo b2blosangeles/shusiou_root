d397ce2e285f097b7afd26edd00272993907a18e
b2blosangeles <b2blosangeles@gmail.com>
2018-12-12 11:41:44 -0800

    Update _commUI.jsx

--- files/app/truefactors/_commUI.jsx
+++ files/app/truefactors/_commUI.jsx
@@ -25,7 +25,7 @@ var _commUI = {
 	pageFrame :  React.createClass({
 		render : function () {
 			var me = this;
-			return (<div className="container wrapper ">
+			return (<div className="container-fluid wrapper ">
 				<div className="row documentPageFrame mt-3 mb-5 p-1">
 					<div className="col-sm-12 p-1 animation-flag">
 					    {(typeof me.props.data === 'string') ? (<span dangerouslySetInnerHTML={{__html: me.props.data}}/>)
