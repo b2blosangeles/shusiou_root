d23ea1f8acd88535135475312e325fafc5a3654a
b2blosangeles <b2blosangeles@gmail.com>
2018-12-12 13:22:35 -0800

    Update homePage.jsx

--- files/app/truefactors/documentPage/homePage.jsx
+++ files/app/truefactors/documentPage/homePage.jsx
@@ -7,14 +7,23 @@ var _homePage = React.createClass({
 	return  (<span>
 		<h3>About Plato Plan</h3>
 		<p>
-		<b>Abstract</b> : PlatoPlan Platform is to inspire every dream maker on the earth. She will provide easy access to
+			<b>Abstract</b> : PlatoPlan Platform is to inspire every dream maker on the earth. She will provide easy access to
   			file and publish innovative concept, ideas, products, works. More importantly, PlatoPlan will help those
 			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
 			and tangible status. The more innovations published on PlatoPlan, more sparkles will be ignited. And it
 			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
 			of the innovations on PlatoPlan. Then its commercial value and social impact will be valueless.	
 		</p>
-
+		<p>
+			<b>Chapter 1 Inventor</b> : PlatoPlan Platform will provide an interface for anyone to register 
+			as member of PlatoPlan. This will include the NDA and legal immunity for PlatoPlan to help file any 
+			concepts, works, ideas, audios, via video, documentation, photograph or other possible means. 
+			PlatoPlan will help the inventors to edit their IP files and publish their edited IP files selected 
+			at their own will. Meanwhile, PlatoPlan will provide interfaces to help the inventor to realize their 
+			IP concepts or finished works into a tangible or industrial or commercial or legal form. PlatoPlan 
+			will be honestly record the time while the file is revealed to the public and thus to protect the 
+			IP rights of the investors..	
+		</p>
 
 		</span>)
     }
