73c0a1c3cd6c977741b60fd354615b69cff4fc8f
b2blosangeles <b2blosangeles@gmail.com>
2018-12-12 13:09:59 -0800

    Update homePage.jsx

--- files/app/truefactors/documentPage/homePage.jsx
+++ files/app/truefactors/documentPage/homePage.jsx
@@ -5,163 +5,18 @@ var _homePage = React.createClass({
     },
     render: function() {
 	return  (<span>
-			<h2>CONFIDENTIALITY AGREEMENT</h2>
-			<p>
-			This Confidentiality Agreement is made at sight by the registered member (“Register”)and
-			Plato Plan Inc. , a company organized under the laws of California, USA (“Plato Plan”).		
-			</p>
-			
-			<p>
-			Plato Plan Inc and Company may be referred to in this Agreement as a “party” or the “parties.”
-			A party that discloses Confidential Information to the other party may be referred to as the
-			“disclosing party,” and the party that receives Confidential Information from the other party may
-			be referred to as the “receiving party.”		
-			</p>
-			
-			<p>
-			In connection with the potential or actual purchase and sale of products and/or services from one
-			party to the other, the parties may disclose Confidential Information (as defined below) to each
-			other and their employees (“Employees”).		
-			</p>
+		<h2>CONFIDENTIALITY AGREEMENT</h2>
+		<p>
+		This Confidentiality Agreement is made at sight by the registered member (“Register”)and
+		Plato Plan Inc. , a company organized under the laws of California, USA (“Plato Plan”).		
+		</p>
 
-			<p>
-			Accordingly, in consideration of the disclosure of Confidential Information to each other, the
-			parties agree as follows:		
-			</p>
-			
-			<p>
-			1. “Confidential Information” means all information concerning the business, affairs,
-			products, research and technologies of the parties that is not publicly available at the time it is
-			disclosed to or learned by the receiving party or its Employees. Confidential Information also
-			includes any information that a party obtains or has obtained from another third party and which
-			the party is obligated to treat as proprietary or confidential.		
-			</p>
-			
-			<p>
-			Confidential Information includes, without limitation, all of the following:
-			</p>
-			<ul>
-				<li>product specifications; data; know-how; formulas; compositions; processes; designs;
-				sketches; photographs; samples; prototypes; inventions; concepts; ideas; past, current and
-				planned research and development; past, current and planned manufacturing or
-				distribution methods and processes;
-				</li>
-				<li>the identity of or other information about actual or potential customers, customer contacts
-				and customer sales strategies;
-				</li>
-				<li>market studies, penetration data and other market information; sales and marketing plans,
-				programs and strategies;
-				</li>
-				<li>sales, costs, and other financial data;
-				</li>
-				<li>the identity of or other information about actual or potential product suppliers and
-				sources of supply for products, raw materials, and components; supplier contact
-				information; plant descriptions and descriptions of production equipment;
-				</li>
-				<li>price lists; business plans; financial reports and statements;
-				</li>				
-				<li>computer software and programs (including object code and source code); databases;
-				</li>
-				<li>internal reports, memoranda, notes, analyses, compilations and studies; and other data,
-				information, materials or intangibles that relate to Sunway’s and/or its customers’
-				business and/or products.
-				</li>	
-			</ul>
-			<p>Confidential Information also includes any material or information that contains or is based on
-			any other Confidential Information, whether prepared by either party or their Employees, or any
-			other person.</p>
-			
-			<p>
-			2. Information and materials will be conclusively deemed to be Confidential Information if
-			marked as confidential or proprietary at the time disclosed. Any information disclosed orally
-			will be conclusively deemed Confidential Information if the disclosing party notifies the
-			receiving party that it is confidential or proprietary at any time following disclosure. A party’s
-			failure, however, to mark information or materials as confidential or proprietary or to notify the
-			other party that oral information is confidential or proprietary will not affect the information’s or
-			material’s confidential or proprietary nature.			
-			</p>
-			
-			<p>
-			3. The parties agree to (a) keep each other’s Confidential Information strictly confidential
-			and disclose it only to those Employees (i) who need to know such Confidential Information in
-			order for the party to explore and facilitate a business relationship with the other party and (ii)
-			who are subject to the same or comparable obligations of confidentiality, (b) use the Confidential
-			Information solely for the purpose of, and only to the extent required for, exploring and
-			facilitating a business relationship with the other party and (c) obtain the other party’s written
-			consent before disclosing any information that could, under any circumstances, constitute
-			Confidential Information.			
-			</p>	
-
-			<p>
-			4. The parties agree that, with respect to the purchase or sale of any products purchased or
-			sold by the disclosing party on or after the date of this agreement, the receiving party may not
-			and shall not solicit any contract, agreement, arrangement, or understanding, directly or
-			indirectly, with any customer of or supplier to the disclosing party whose identity is disclosed to
-			receiving party by disclosing party.			
-			</p>
-
-			<p>
-			5. The obligations and limitations set forth herein regarding Confidential Information shall
-			not apply to information that is:			
-			</p>
-			
-			<ul>
-				<li>at any time in the public domain other than by a breach of this Agreement by the
-				party who received such Confidential Information;
-				</li>
-				<li>at any time received from a third party that has the right to furnish it to the party
-				without restrictions on disclosure or use;
-				</li>
-				<li>known to the party without any restriction on disclosure or use prior to its receipt
-				from the other party or independently developed by the party; or
-				</li>	
-				<li>authorized in writing by the disclosing party to be disclosed by the other party.
-				</li>	
-			</ul>
-			
-			<p>
-			6. Confidential Information supplied shall not be reproduced in any form by the receiving
-			party or its Employees except as required to accomplish the purpose of this Agreement.			
-			</p>
-			
-			<p>
-			7. Upon the request of the disclosing party, the receiving party agrees to return all
-			documents and materials containing any Confidential Information, as well as all copies thereof.		
-			</p>
-			
-			<p>
-			8. The parties will cause all of their Employees to comply with the terms of this Agreement.
-			Each party will be liable and responsible for any violation of the terms of this Agreement by any
-			of its Employees. A party will immediately inform the other party of any breach of this
-			Agreement by any of its Employees, and the party will take all steps needed to halt any current
-			violation and prevent future violations of this Agreement.			
-			</p>
-
-			<p>
-			9. Neither party shall have any commitment or obligation to enter into any further
-			agreement with the other party or to refrain from dealing with any third party in any manner
-			whatsoever, except as expressly agreed by the parties in writing.			
-			</p>
-			
-			<p>
-			10. This Agreement is limited to terms and conditions governing the disclosure and use of
-			Confidential Information, and no supply or other commercial relationship or obligation between
-			the parties is created by this Agreement.			
-			</p>
-			
-			<p>
-			11. This Agreement contains the entire agreement of the parties concerning the subject matter
-			hereof. No modification or waiver of the terms of this Agreement will be binding unless in
-			writing and signed by Sunway and Company.			
-			</p>
-			
-			<p>
-			12. This Agreement will be governed by the laws of the State of Minnesota applicable to
-			contracts executed in and to be performed in the State of Minnesota. The parties hereby submit
-			to the jurisdiction of any state or federal courts located in the State of Minnesota. The parties
-			agree that monetary damages are an inadequate remedy for a breach of this Agreement and that
-			injunctive relief is an appropriate additional remedy.			
-			</p>
+		<p>
+		Plato Plan Inc and Company may be referred to in this Agreement as a “party” or the “parties.”
+		A party that discloses Confidential Information to the other party may be referred to as the
+		“disclosing party,” and the party that receives Confidential Information from the other party may
+		be referred to as the “receiving party.”		
+		</p>
 		</span>)
     }
 });
