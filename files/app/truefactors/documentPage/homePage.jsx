eab7de69fb1a2f20bc32870604b46b59ed83014d
b2blosangeles <b2blosangeles@gmail.com>
2018-12-12 12:03:57 -0800

    Update homePage.jsx

--- files/app/truefactors/documentPage/homePage.jsx
+++ files/app/truefactors/documentPage/homePage.jsx
@@ -5,59 +5,40 @@ var _homePage = React.createClass({
     },
     render: function() {
 	return  (<span>
-			<h3>Home Page</h3>
+			<h2>CONFIDENTIALITY AGREEMENT</h2>
 			<p>
-			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
-			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
-			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
-			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
-			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
-			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			This Confidentiality Agreement is made at sight by the registered member (“Register”)and
+			Plato Plan Inc. , a company organized under the laws of California, USA (“Plato Plan”).		
 			</p>
 			
 			<p>
-			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
-			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
-			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
-			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
-			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
-			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			Plato Plan Inc and Company may be referred to in this Agreement as a “party” or the “parties.”
+			A party that discloses Confidential Information to the other party may be referred to as the
+			“disclosing party,” and the party that receives Confidential Information from the other party may
+			be referred to as the “receiving party.”		
 			</p>
 			
 			<p>
-			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
-			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
-			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
-			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
-			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
-			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			In connection with the potential or actual purchase and sale of products and/or services from one
+			party to the other, the parties may disclose Confidential Information (as defined below) to each
+			other and their employees (“Employees”).		
 			</p>
 
 			<p>
-			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
-			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
-			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
-			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
-			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
-			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			Accordingly, in consideration of the disclosure of Confidential Information to each other, the
+			parties agree as follows:		
 			</p>
 			
 			<p>
-			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
-			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
-			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
-			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
-			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
-			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			1. “Confidential Information” means all information concerning the business, affairs,
+			products, research and technologies of the parties that is not publicly available at the time it is
+			disclosed to or learned by the receiving party or its Employees. Confidential Information also
+			includes any information that a party obtains or has obtained from another third party and which
+			the party is obligated to treat as proprietary or confidential.		
 			</p>
 			
 			<p>
-			Balabala Platform is to inspire every dream maker on the earth. She will provide easy access to
-			file and publish innovative concept, ideas, products, works. More importantly, Balabala will help those
-			inventors to go through all the necessary processes to turn their original concept or ideas into a practical
-			and tangible status. The more innovations published on Balabala, more sparkles will be ignited. And it
-			will attract individuals’ inventors, investors, enterprises and normal human beings, to search the origins
-			of the innovations on Balabala. Then its commercial value and social impact will be valueless.		
+			Confidential Information includes, without limitation, all of the following:		
 			</p>
 		</span>)
     }
