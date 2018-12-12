7b4f3f50db5920a0d18f2e5cda8d7fa070e32c1c
b2blosangeles <b2blosangeles@gmail.com>
2018-12-12 07:25:14 -0800

    Update _classAuth.jsx

--- files/app/truefactors/_classAuth.jsx
+++ files/app/truefactors/_classAuth.jsx
@@ -48,7 +48,7 @@ React.createClass({
 						<label className="custom-control-label" for="customCheck1">Remember password</label>
 					      </div>
 					      <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-					      <hr className="my-4">
+					      <hr className="my-4"/>
 					      <button className="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i className="fab fa-google mr-2"></i> Sign in with Google</button>
 					      <button className="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i className="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
 					    </form>
