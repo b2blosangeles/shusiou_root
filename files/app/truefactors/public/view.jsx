c3905a5af32a79153dd5e62d590738bbd9618232
b2blosangeles <b2blosangeles@gmail.com>
2018-12-13 09:31:24 -0800

    Update view.jsx

--- files/app/truefactors/public/view.jsx
+++ files/app/truefactors/public/view.jsx
@@ -12,8 +12,6 @@ var _compView = function(me, Root) {
 		};
 		return (<span>	
 		<button type="button" className="btn btn-info" onClick={me.compData.loadVideos.bind(me)}>Load Data</button>
-		&nbsp;
-		<button type="button" className="btn btn-info" onClick={Root.overLay.popup.bind(me, popupSetting)}>Popup Window</button>
 		</span>)
 	};
 	this.publicSearch = function () {
