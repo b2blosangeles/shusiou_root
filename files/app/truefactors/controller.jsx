49b9c1c15b45f04895954a399f89db9a7ccecb70
b2blosangeles <b2blosangeles@gmail.com>
2018-12-13 10:36:50 -0800

    Update controller.jsx

--- files/app/truefactors/controller.jsx
+++ files/app/truefactors/controller.jsx
@@ -6,6 +6,8 @@ React.createClass({
         },
 	getAuth : function() {
 		var me = this;
+		__asyncOId = (!__asyncOId || __asyncOId > 1000000) ? 1 : (__asyncOId + 1);
+		var _oId = __asyncOId + '-' + new Date().getTime();
 		return (
 			<_asyncModule plugin={{
 			extend: {
