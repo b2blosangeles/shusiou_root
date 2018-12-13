6f63518622f3d0e5503c511b4dfb513d1bfbadef
b2blosangeles <b2blosangeles@gmail.com>
2018-12-13 09:38:55 -0800

    Update view.jsx

--- files/app/truefactors/public/view.jsx
+++ files/app/truefactors/public/view.jsx
@@ -15,7 +15,6 @@ var _compView = function(me, Root) {
 		</span>)
 	};
 	this.publicSearch = function () {
-		alert(111);
 		return  (
 			<div className="container">
 				{Root.lib.spinAnchor(me, 'AA')}
