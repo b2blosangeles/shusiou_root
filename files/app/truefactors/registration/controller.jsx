e09bb64870342150c9ed1ca7afb3ad1f261f25b0
b2blosangeles <b2blosangeles@gmail.com>
2018-12-12 16:25:46 -0800

    Update controller.jsx

--- files/app/truefactors/registration/controller.jsx
+++ files/app/truefactors/registration/controller.jsx
@@ -17,7 +17,7 @@ React.createClass({
 
                     <label for="psw-repeat"><b>Repeat Password</b></label>
                     <input type="password" placeholder="Repeat Password" name="psw-repeat" required/>
-                    <hr>
+                    <hr/>
 
                     <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
                     <button type="submit" className="registerbtn">Register</button>
