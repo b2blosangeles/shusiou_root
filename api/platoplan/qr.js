a3ff919fb656f77593fc6c8e8e06d23b2e0810b8
b2blosangeles <b2blosangeles@gmail.com>
2018-12-13 21:35:55 -0800

    Update qr.js

--- api/platoplan/qr.js
+++ api/platoplan/qr.js
@@ -2,7 +2,7 @@
 var CP  = require(env.root_path + "/package/crowdProcess/crowdProcess.js");
 var qr = require(env.root_path + "/vendor/qr-image/node_modules/qr-image/");
 
-var code = qr.image("text to show in qr", { type: 'png', ec_level: 'H', size: 10, margin: 0 });
+var code = qr.image("http://dev.platoplan.com/api/sz.api?code=1234", { type: 'png', ec_level: 'H', size: 15, margin: 1 });
 res.type('png');
 code.pipe(res);
 
