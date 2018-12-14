98361b1cf94b2d75a36da97697814591e6f754c9
b2blosangeles <b2blosangeles@gmail.com>
2018-12-14 08:24:10 -0800

    Update qr.js

--- api/platoplan/qr.js
+++ api/platoplan/qr.js
@@ -2,7 +2,7 @@
 var CP  = require(env.root_path + "/package/crowdProcess/crowdProcess.js");
 var qr = require(env.root_path + "/vendor/qr-image/node_modules/qr-image/");
 var crypto = require('crypto');
-var hash = crypto.createHash('md5').update('string').digest('hex');
+var hash = crypto.createHash('md5').update('').digest('hex');
 res.send(hash);
 return true;
 var code = qr.image("http://dev.platoplan.com/api/platoplan/auth.api?code=66547656", { type: 'png', ec_level: 'H', size: 10, margin: 1 });
