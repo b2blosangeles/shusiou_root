da1bc23ec95fe9c989d133aa766e802e3e8a5d27
b2blosangeles <b2blosangeles@gmail.com>
2018-12-14 16:06:09 -0800

    Update auth.js

--- api/platoplan/auth.js
+++ api/platoplan/auth.js
@@ -1,7 +1,7 @@
 var Smarty = require(env.site_path + '/api/inc/jsmart/smart.min.js'); 
 // res.send({success: true, code: (!req.query.code) ? '' : req.query.code});
 
-var indextpl = env.site_path + '/api/platoplan/tpl/index.tpl';
+var indextpl = env.site_path + '/api/platoplan/tpl/login.html';
 
 pkg.fs.readFile(indextpl, 'utf-8', function(err, content) {	
 			var tpl = new Smarty(content);
