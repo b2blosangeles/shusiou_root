7ed7d3ba64c5b49589f0e68abc80b261fd3190ab
b2blosangeles <b2blosangeles@gmail.com>
2018-12-18 13:03:57 -0800

    Update auth.js

--- api/platoplan/auth.js
+++ api/platoplan/auth.js
@@ -3,19 +3,23 @@ var crypto = require('crypto');
 
 var token = (!req.query.code) ? '' : req.query.code;
 
-var type = (['signin', 'registration'].indexOf(req.query.type) === -1) ? null : req.query.type;
+var type = (['signin', 'registration', 'error'].indexOf(req.query.type) === -1) ? null : req.query.type;
 if (!type) {
   res.send('Wrong type!!');
   return true;
 }
-
 var indextpl = env.site_path + '/api/platoplan/tpl/' + type + '.html';
 
-var s = Math.random().toString(36).substr(2, 16) + new Date().getTime();
-var key = crypto.createHash('md5').update(s).digest('hex');
+
 
 pkg.fs.readFile(indextpl, 'utf-8', function(err, content) {	
-			var tpl = new Smarty(content);
-			res.send(tpl.fetch({token : token, key : key}));
-			return true;
-	});
+	var tpl = new Smarty(content);
+	if (type === 'error') {
+		res.send(tpl.fetch({}));
+	} else {
+		var s = Math.random().toString(36).substr(2, 16) + new Date().getTime();
+		var key = crypto.createHash('md5').update(s).digest('hex');
+		res.send(tpl.fetch({token : token, key : key}));
+		return true;
+	}
+});
