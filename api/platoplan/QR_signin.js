b2ab72f8ef842fb5b5bea97af287f8131dfdb3b7
b2blosangeles <b2blosangeles@gmail.com>
2018-12-18 09:37:17 -0800

    Update QR_signin.js

--- api/platoplan/QR_signin.js
+++ api/platoplan/QR_signin.js
@@ -1,1 +1,11 @@
-res.send('QR_signin.js');
+
+var CP  = require(env.root_path + "/package/crowdProcess/crowdProcess.js");
+var qr = require(env.root_path + "/vendor/qr-image/node_modules/qr-image/");
+var crypto = require('crypto');
+var s = Math.random().toString(36).substr(2, 16) + new Date().getTime();
+var hash = crypto.createHash('md5').update(s).digest('hex');
+
+var code = qr.image('http://dev.platoplan.com/api/platoplan/auth.api?code=' + hash, 
+                    { type: 'png', ec_level: 'H', size: 8, margin: 1 });
+res.type('png');
+code.pipe(res);
