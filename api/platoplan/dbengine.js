9d997f6909d3c19ec0d40e1be315b2b07c056493
b2blosangeles <b2blosangeles@gmail.com>
2018-12-18 08:02:05 -0800

    Update dbengine.js

--- api/platoplan/dbengine.js
+++ api/platoplan/dbengine.js
@@ -2,5 +2,19 @@
 var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql');
 var config = require(env.config_path + '/config.json');
 var CP = new pkg.crowdProcess();
-var cfg0 =config.db;
-res.send('cfg0');
+var db_setting = config.db;
+db_setting.database = 'platoplan';
+var connection = mysql.createConnection(db_setting);
+var str = 'INSERT show DATABASES; ';
+
+connection.connect();
+connection.query(str, function (error, results, fields) {
+connection.end();	
+  if (!error) {
+    res.send(results);
+    return true;
+  } else {
+    res.send(error.message);
+  }
+}); 
+
