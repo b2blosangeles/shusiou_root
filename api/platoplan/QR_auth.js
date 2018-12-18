1195b0874006905f505e447a6c12d6a2ae48ab43
b2blosangeles <b2blosangeles@gmail.com>
2018-12-18 12:06:05 -0800

    Update and rename QR_signin.js to QR_auth.js

--- api/platoplan/QR_auth.js
+++ api/platoplan/QR_auth.js
@@ -3,7 +3,7 @@ var CP  = require(env.root_path + "/package/crowdProcess/crowdProcess.js");
 var qr = require(env.root_path + "/vendor/qr-image/node_modules/qr-image/");
 var crypto = require('crypto');
 
-var type = (['sign', 'registration'].indexOf(req.query.type) === -1) ? null : req.query.type;
+var type = (['signin', 'registration'].indexOf(req.query.type) === -1) ? null : req.query.type;
 if (!type) {
   res.send('Wrong type!!');
   return true;
