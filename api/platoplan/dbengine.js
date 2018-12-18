149b23fb03a82cd02d06641b413ac9d3fd4d9048
b2blosangeles <b2blosangeles@gmail.com>
2018-12-18 07:59:20 -0800

    Update dbengine.js

--- api/platoplan/dbengine.js
+++ api/platoplan/dbengine.js
@@ -3,4 +3,4 @@ var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql');
 var config = require(env.config_path + '/config.json');
 var CP = new pkg.crowdProcess();
 var cfg0 =config.db;
-res.send(cfg0);
+res.send('cfg0');
