c302d74481e3c1885d4820e610137d8a48670c2b
b2blosangeles <b2blosangeles@gmail.com>
2018-12-19 13:08:40 -0800

    Update controller.jsx

--- files/app/truefactors/auth/controller.jsx
+++ files/app/truefactors/auth/controller.jsx
@@ -6,24 +6,35 @@ React.createClass({
                 }
 		return {}
         },        
-        sendFrom : function (menuItem) {
+        sendLoginFrom : function () {
           var me = this;
           return  (<div className="container">
 			<div className="row">
 				<div className="col-sm-12 p-5 text-center">
-					<h5>Use iphone to scan the QR code to start registration --{menuItem} --</h5>
+					<h5>Use iphone to scan the QR code to start login</h5>
 					<img src="http://dev.platoplan.com/api/platoplan/qr.api" className="mt-3"/>
 				</div>	  
 			</div>	  
             	</div>)        
         },
+        sendRegistrationFrom : function () {
+          var me = this;
+          return  (<div className="container">
+			<div className="row">
+				<div className="col-sm-12 p-5 text-center">
+					<h5>Use iphone to scan the QR code to start registration</h5>
+					<img src="http://dev.platoplan.com/api/platoplan/qr.api" className="mt-3"/>
+				</div>	  
+			</div>	  
+            	</div>)        
+        },	
         render: function() {
                 var me = this;
 		var menuItem = (!me.props.data) ? 'homePage' : me.props.data;
                 return Root.commUI.show({
                                 code: 'infoBox', 
                                 parent : me, 
-                                data : me.sendFrom(menuItem), 
+                                data : (menuItem == 'login') ? me.sendLoginFrom() : me.sendRegistrationFrom() , 
                                 setting : {
                                         type : 'light',
                                         noshadow : true,
