5c52010b17468fb7a976946876064cf378e8db4b
b2blosangeles <b2blosangeles@gmail.com>
2018-12-19 13:57:35 -0800

    Update QR_auth.js

--- api/platoplan/QR_auth.js
+++ api/platoplan/QR_auth.js
@@ -1,3 +1,7 @@
+var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql');
+var db_setting = config.db;
+db_setting.database = 'platoplan';
+
 
 var CP  = require(env.root_path + "/package/crowdProcess/crowdProcess.js");
 var qr = require(env.root_path + "/vendor/qr-image/node_modules/qr-image/");
