3da576d00e2028c5c9b4aaff5e5b6c15a204a6b1
b2blosangeles <b2blosangeles@gmail.com>
2018-12-20 12:38:47 -0800

    Update controller.jsx

--- files/app/dbadmin/controller.jsx
+++ files/app/dbadmin/controller.jsx
@@ -2,10 +2,33 @@ React.createClass({
         getInitialState: function() {
 	        var me = this;
                 me.result = {};
+		me.tables = [];
+		me.callQuery('SHOW TABLES', me.showTables);
 	        return {}    
         },
-	
-        sendQuery : function() {
+       showTables : function(data) {
+	    var me = this; 
+	    mr.tables = [];
+	    console.log(data);
+	       
+        },
+        callQuery : function(query, cbk) {
+	    var me = this;
+            $.ajax({
+                url: '/api/platoplan/dbengine.api',
+                dataType: 'json',
+                type: 'post',
+                contentType: 'application/x-www-form-urlencoded',
+                data: {query : query},
+                success: function( data, textStatus, jQxhr ){
+			cbk(data.data); 
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log( errorThrown );
+                }
+            });                
+        },	
+        submitQuery : function() {
 	    var me = this;
 		var query = false, cbk = false;
             $.ajax({
@@ -41,7 +64,7 @@ React.createClass({
                                       
                               </div>
                               <div className="col-sm-1">
-                                   <button className="btn btn-default border border-default" onClick={me.sendQuery.bind(me)} type="submit">submit</button>
+                                   <button className="btn btn-default border border-default" onClick={me.submitQuery.bind(me)} type="submit">submit</button>
                               </div> 
                           </div>
                            <div className="row">
