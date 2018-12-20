7fd74bcd6319aaa3f25ac0f7c85dc73a2d85eb3e
b2blosangeles <b2blosangeles@gmail.com>
2018-12-19 16:36:05 -0800

    Update auth.js

--- api/platoplan/auth.js
+++ api/platoplan/auth.js
@@ -42,7 +42,7 @@ CP.serial(
 			return true;
 		});
 	 }
-      	res.send({_spent_time:data._spent_time, status:data.status, data:data});
+      	//res.send({_spent_time:data._spent_time, status:data.status, data:data});
   },
   30000
 );
