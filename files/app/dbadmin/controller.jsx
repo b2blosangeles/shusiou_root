12cf25474a34cc06b2ac640a6b85b2dc8f22352d
b2blosangeles <b2blosangeles@gmail.com>
2018-12-20 09:10:18 -0800

    Create controller.jsx

---
+++ files/app/dbadmin/controller.jsx
@@ -0,0 +1,26 @@
+React.createClass({
+        getInitialState: function() {
+        render: function() {
+        	var me = this;
+          return  (
+          <span>
+              <div className="container">
+                  <div className="row">
+                      <div className="col-sm-3">
+                        Table
+                      </div>
+                      <div className="col-sm-9">
+                        Result
+                      </div>
+                  </div>
+                   <div className="row">
+                      <div className="col-sm-3">
+                      </div>
+                      <div className="col-sm-9">
+                      </div>
+                  </div>
+              </div>
+          </span>
+          )
+              }
+})
