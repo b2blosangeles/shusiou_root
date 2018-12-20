ffa230ccfbdfe725d6bcf02542d31ede3442faa6
b2blosangeles <b2blosangeles@gmail.com>
2018-12-20 09:59:08 -0800

    Update controller.jsx

--- files/app/dbadmin/controller.jsx
+++ files/app/dbadmin/controller.jsx
@@ -3,11 +3,13 @@ React.createClass({
         	var me = this;
                 return  (<div className="container px-1">
                           <div className="row">
-                              <div className="col-sm-3">
+                              <div className="col-sm-2">
                                 Tables<hr/>
                               </div>
-                              <div className="col-sm-9">
-                                        <textarea className="form-control" rows="5"></textarea>
+                              <div className="col-sm-10">
+                                   <textarea className="form-control" rows="5"></textarea>
+                                   </br>
+                                   <botton className="btn btn-default" type="submit">submit</button>
                               </div>
                           </div>
                            <div className="row">
