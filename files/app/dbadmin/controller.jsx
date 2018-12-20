ad64289f85bb5de5bccb405e8ac64574e253fd2b
b2blosangeles <b2blosangeles@gmail.com>
2018-12-20 15:56:12 -0800

    Update controller.jsx

--- files/app/dbadmin/controller.jsx
+++ files/app/dbadmin/controller.jsx
@@ -82,7 +82,7 @@ React.createClass({
                                    [{JSON.stringify(me.result)}]
                                       
                               </div>
-                              <div className="col-sm-1">
+                              <div className="col-sm-1 p-3">
                                    <button className="btn btn-default border border-default" onClick={me.submitQuery.bind(me)} type="submit">submit</button>
                               </div> 
                           </div>
