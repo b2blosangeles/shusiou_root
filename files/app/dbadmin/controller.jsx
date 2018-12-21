f697c7799b32e5984cad129560d951fed3d378b1
b2blosangeles <b2blosangeles@gmail.com>
2018-12-21 12:29:42 -0800

    Update controller.jsx

--- files/app/dbadmin/controller.jsx
+++ files/app/dbadmin/controller.jsx
@@ -85,8 +85,8 @@ React.createClass({
         },
         render: function() {
         	var me = this;
-                return  (<div className="container" style={{'padding':'0px'}}>
-                          <div className="row mb-1 border">
+                return  (<div className="container">
+                          <div className="row mb-1 border" style={{'padding':'0px'}}>
                               <div className="col-sm-12" style={{'margin':'0px'}}> 
 				<div className="card p-2 w-100 alert-success" style={{'margin':'0px'}}>
 				MYSQL Viewer
