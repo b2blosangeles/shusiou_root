2f25a77d652a2e39a637709b2f84e12fdfd01e27
b2blosangeles <b2blosangeles@gmail.com>
2018-12-21 15:57:15 -0800

    Update controller.jsx

--- files/app/dbadmin/controller.jsx
+++ files/app/dbadmin/controller.jsx
@@ -158,7 +158,7 @@ React.createClass({
 						   <div className="row" style={{'margin':'0px', 'padding':'0px'}}>
 							   <div className="col-sm-11">&nbsp;</div>
 							   <div className="col-sm-1">
-					   			<button className="btn btn-warning d-flex w-100 border border-dark" 
+					   			<button className="btn btn-warning d-flex border border-dark" 
 						   		onClick={me.submitQuery.bind(me)} type="submit">Submit</button>
 							   </div>
 						   </div>	   
