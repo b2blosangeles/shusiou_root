1efd963f05d969fcc87c741063ee63c3ad02d4a4
b2blosangeles <b2blosangeles@gmail.com>
2018-12-21 14:38:44 -0800

    Update controller.jsx

--- files/app/dbadmin/controller.jsx
+++ files/app/dbadmin/controller.jsx
@@ -86,7 +86,7 @@ React.createClass({
 	showData : function(data) {
 		var fields = (!data || !data.length) ? [] : Object.keys(data[0]);
 		return (!data || !data.length) ?  (<span></span>) :
-		(<div className="pre-scrollable alert-warning" style={{"min-height":"28em"}}>
+		(<div className="pre-scrollable alert-secondary" style={{"min-height":"28em"}}>
 		<table className="table alert-warning pre-scrollable">
 		  <thead><tr>
 			{fields.map(
