9de850d92946d73b1d822f0d467502f137a2423c
b2blosangeles <b2blosangeles@gmail.com>
2018-12-20 17:06:20 -0800

    Update controller.jsx

--- files/app/dbadmin/controller.jsx
+++ files/app/dbadmin/controller.jsx
@@ -63,8 +63,8 @@ React.createClass({
                 return  (<div className="container">
                           <div className="row mb-1 alert-success border">
                               <div className="col-sm-2"> 
-				<div className="btn-group mt-2">
-				  <button type="button" className="btn btn-info btn-lg btn-block dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				<div className="btn-group btn-block mt-2">
+				  <button type="button" className="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				    Action
 				  </button>
 				  <div className="dropdown-menu">
