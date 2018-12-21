e71a79142a9b6dad6c19e887c31a5b098cc986f7
b2blosangeles <b2blosangeles@gmail.com>
2018-12-21 09:05:52 -0800

    Update controller.jsx

--- files/app/dbadmin/controller.jsx
+++ files/app/dbadmin/controller.jsx
@@ -95,7 +95,7 @@ React.createClass({
                           
                           </div>				
                           <div className="row alert-secondary">
-				<div className="col-sm-3 p-2"> 
+				<div className="col-sm-3"> 
 					<div className="btn-group d-flex w-100 mt-2">
 					  <button type="button" className="btn btn-info  w-100 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						  {(me.state.currentDB) ? me.state.currentDB : 'Select Database'}
