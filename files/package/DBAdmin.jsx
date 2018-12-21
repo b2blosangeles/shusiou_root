6c04f9658999318b313f316fc6c28ec26488a7cd
b2blosangeles <b2blosangeles@gmail.com>
2018-12-21 13:28:40 -0800

    Update DBAdmin.jsx

--- files/package/DBAdmin.jsx
+++ files/package/DBAdmin.jsx
@@ -7,8 +7,6 @@
 		"/js/react/react-dom.min.js",
 		"/js/react/react-cookie.0.4.9.min.js",
 		"/js/react/ReactRouter/3.0.2/ReactRouter.min.js",
-		"/js/ReactPlugin.js",
-		"/css/Bootstrap/v4.1.3/bootstrap.min.js",
-		"/css/Bootstrap/v4.1.3/popper.min.js"
+		"/js/ReactPlugin.js"
 	]
 }
