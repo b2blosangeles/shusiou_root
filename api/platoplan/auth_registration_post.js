e17a679ed667099d8a43572eb7a604d82ec2476b
b2blosangeles <b2blosangeles@gmail.com>
2018-12-27 14:45:22 -0800

    Update auth_registration_post.js

--- api/platoplan/auth_registration_post.js
+++ api/platoplan/auth_registration_post.js
@@ -1,1 +1,23 @@
-res.send(req.body);
+var token = (!req.body.token) ? '' : req.body.token;
+var CP = new pkg.crowdProcess();
+
+var _f = {};
+_f['DBS'] = function(cbk) {
+	var connection = mysql.createConnection(db_setting);
+	connection.connect();
+
+	var str = "SELECT * FROM  `QR` WHERE `token` = '" + token + "'";	
+	connection.query(str, function (error, results, fields) {
+	      connection.end();	
+	      if (!error && (results.length)) {
+		  cbk({success: true});
+	      } else {
+		  cbk({success: false});
+	      }
+	}); 
+}
+CP.serial(
+  _f,
+  function(data) {
+      res.send({body:req.body, data: data});
+  });  
