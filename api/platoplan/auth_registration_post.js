81ef8e446d57f9881999999352a39fe1c50df69a
b2blosangeles <b2blosangeles@gmail.com>
2018-12-27 18:16:34 -0800

    Update auth_registration_post.js

--- api/platoplan/auth_registration_post.js
+++ api/platoplan/auth_registration_post.js
@@ -31,8 +31,8 @@ _f['AddUser'] = function(cbk) {
 	    UserName = req.body.UserName,
 	    Password = crypto.createHash('md5').update(s).digest('hex');
 	
-	var str = "INSERT INTO  `user` (`username`, `email`, `password`) " + 
-	    " VALUES ('" + UserName + "','" + Email + "','" + Password + "')";	
+	var str = 'INSERT INTO  `user` (`username`, `email`, `password`) ' + 
+	    ' VALUES ("' + UserName + '","' + Email + '","' + Password + '")';	
 	
 	connection.query(str, function (error, results, fields) {
 	      connection.end();	
