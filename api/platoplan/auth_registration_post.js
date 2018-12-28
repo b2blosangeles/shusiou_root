b7733e042e5a6bd4d5b7071bfc1f1938176b8b40
b2blosangeles <b2blosangeles@gmail.com>
2018-12-27 17:56:27 -0800

    Update auth_registration_post.js

--- api/platoplan/auth_registration_post.js
+++ api/platoplan/auth_registration_post.js
@@ -5,6 +5,8 @@ var config = require(env.config_path + '/config.json');
 var db_setting = config.db;
 db_setting.database = 'platoplan';
 
+var crypto = require('crypto');
+
 var _f = {};
 _f['DBS'] = function(cbk) {
 	var connection = mysql.createConnection(db_setting);
@@ -23,8 +25,15 @@ _f['DBS'] = function(cbk) {
 _f['AddUser'] = function(cbk) {
 	var connection = mysql.createConnection(db_setting);
 	connection.connect();
-
-	var str = "SELECT * FROM  `QR` WHERE `token` = '" + token + "'";	
+	var s = Math.random().toString(36).substr(2, 16) + new Date().getTime();
+	
+	var Email = req.body.Email,  
+	    UserName = req.body.UserName,
+	    Password = crypto.createHash('md5').update(s).digest('hex');
+	
+	var str = "INSERT INTO  `user` (`username`, `email`, `password`) " + 
+	    " VALUES ('" + UserName + "','" + Email + "','" + Password + ")";	
+	
 	connection.query(str, function (error, results, fields) {
 	      connection.end();	
 	      if (!error && (results.length)) {
