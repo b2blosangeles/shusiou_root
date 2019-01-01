565b844b914f08ef1fcb0e612d78cb24019148b7
b2blosangeles <b2blosangeles@gmail.com>
2018-12-31 16:30:42 -0800

    Update controller.jsx

--- files/app/truefactors/investors/controller.jsx
+++ files/app/truefactors/investors/controller.jsx
@@ -22,7 +22,11 @@ React.createClass({
 		var me = this;
 		if (me.props.parent.state.menuOption === 'myVideos') {
 			return me.compView.showVideos();
-		}  else if (me.props.parent.state.menuOption === 'howToStartInvestment'  || !me.props.parent.state.menuOption) {
+		}
+		if (me.props.parent.state.menuOption === 'myVideo') {
+			return 'me.compView.showVideos()';
+		}
+		if (me.props.parent.state.menuOption === 'howToStartInvestment'  || !me.props.parent.state.menuOption) {
 			return me.compContents['howToStartInvestment'];
 		} else {	
 			return (<span>
