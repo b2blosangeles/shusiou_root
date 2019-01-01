dccbab128b3c121abb3ebfe66a3acd1f1658f987
b2blosangeles <b2blosangeles@gmail.com>
2018-12-31 20:24:09 -0800

    Update view.jsx

--- files/app/truefactors/inventors/view.jsx
+++ files/app/truefactors/inventors/view.jsx
@@ -155,7 +155,7 @@ var _compView = function(me, Root) {
 				)
 		};
 		return (<a href="JavaScript: void(0)" onClick={Root.overLay.popup.bind(me, popupSetting)}>
-				<i className="fa fa-pencil" aria-hidden="true"></i>
+				<i className="fa fa-scissors" aria-hidden="true"></i>
 				<br/>
 				<img className="w-100" src={url}/></a>);
 	}
