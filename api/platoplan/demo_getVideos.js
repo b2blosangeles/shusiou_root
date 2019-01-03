51fc62de1430f6ee83f33f662b8b477f8dfd02a1
b2blosangeles <b2blosangeles@gmail.com>
2019-01-03 14:39:18 -0800

    Update demo_getVideos.js

--- api/platoplan/demo_getVideos.js
+++ api/platoplan/demo_getVideos.js
@@ -1,1 +1,9 @@
-res.send(env.root_path + '/demo_videos');
+let list = [],
+    dirn = env.root_path + '/demo_videos';
+
+pkg.fs.readdir(dirn, (err, files) => {
+    files.forEach(file => {
+        list.push(file);
+    });
+    res.send(list);
+});
