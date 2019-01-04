f88a1148ed109cadca279d13e276b07b0633e73d
b2blosangeles <b2blosangeles@gmail.com>
2019-01-04 12:16:42 -0800

    Update view.jsx

--- files/app/truefactors/inventors/view.jsx
+++ files/app/truefactors/inventors/view.jsx
@@ -121,27 +121,7 @@ var _compView = function(me, Root) {
 				<img className="w-100" src={url}/></a>);
 	}
 	this.showImagePage = function(i) {
-		var list = [
-			'/app/truefactors/resource/webp/mqdefault_6s.webp',
-			'/app/truefactors/resource/webp/100.webp',
-			'/app/truefactors/resource/webp/101.webp',
-			'/app/truefactors/resource/webp/102.webp',
-			'/app/truefactors/resource/webp/103.webp',
-			'/app/truefactors/resource/webp/104.webp',
-			'/app/truefactors/resource/webp/105.webp',
-			'/app/truefactors/resource/webp/106.webp',
-			'/app/truefactors/resource/webp/107.webp',
-			'/app/truefactors/resource/webp/108.webp',
-			'/app/truefactors/resource/webp/109.webp',
-			'/app/truefactors/resource/webp/110.webp',
-			'/app/truefactors/resource/webp/111.webp',
-			'/app/truefactors/resource/webp/112.webp',
-			'/app/truefactors/resource/webp/113.webp',
-			'/app/truefactors/resource/webp/114.webp',
-			'/app/truefactors/resource/webp/115.webp',
-			'/app/truefactors/resource/webp/116.webp',
-			'/app/truefactors/resource/webp/117.webp'
-		]
+		var list = me.list;
 		
 		var item = list[Math.floor(Math.random()*list.length)];
 		var url = (list[i]) ? list[i] : item;
