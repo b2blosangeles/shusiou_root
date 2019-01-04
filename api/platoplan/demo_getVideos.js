81d6311ebea4250442129ee8bb29504c7b425b2d
b2blosangeles <b2blosangeles@gmail.com>
2019-01-03 16:31:45 -0800

    Update demo_getVideos.js

--- api/platoplan/demo_getVideos.js
+++ api/platoplan/demo_getVideos.js
@@ -1,6 +1,8 @@
 let list = [],
     dirn = env.root_path + '/demo_videos';
 
+var CP = new pkg.crowdProcess();
+
 pkg.fs.readdir(dirn, (err, files) => {
     files.forEach(file => {
         if (/\.mp4$/.test(file)) {
