9494ba5d47f2c2a5f877a7e45008eb0c9d2ba2cb
b2blosangeles <b2blosangeles@gmail.com>
2019-01-06 09:22:15 -0800

    Update viewVideo.jsx

--- files/app/truefactors/inventors/viewVideo.jsx
+++ files/app/truefactors/inventors/viewVideo.jsx
@@ -7,7 +7,7 @@ var _compViewVideo = function(me, Root) {
 	this.setSubModule = function(v) {
 		me.setState({subModule : v});
 	};
-	this.showDigitizeStream() {
+	this.showDigitizeStream = functioin() {
 		var list = me.DigitizeStreamList;
 		return (
 			<div className="container">
