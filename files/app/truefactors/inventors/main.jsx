b3c1d4b4c4da00a853a1641b0bc524502bd31c1e
b2blosangeles <b2blosangeles@gmail.com>
2019-01-06 10:27:58 -0800

    Update main.jsx

--- files/app/truefactors/inventors/main.jsx
+++ files/app/truefactors/inventors/main.jsx
@@ -6,7 +6,7 @@ React.createClass({
 		me.compViewInvention = new _compViewInvention(me, Root);
 		me.compViewVideo = new _compViewVideo(me, Root);
 		me.compContents = _compContents;
-		me.DigitizeStreamList = Root.lib.getNumberList(16);
+		me.DigitizeStreamList = Root.lib.getNumberList(20);
           	return {}
         },
 	componentDidMount : function() {
