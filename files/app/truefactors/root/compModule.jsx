de7eceb9d43aee701f57d34b8d462c7cd55a14b1
b2blosangeles <b2blosangeles@gmail.com>
2019-01-05 20:58:41 -0800

    Update compModule.jsx

--- files/app/truefactors/root/compModule.jsx
+++ files/app/truefactors/root/compModule.jsx
@@ -45,7 +45,8 @@ var _compModule = function(me, Root) {
 					},
 					includes : [
 						'https://dev.shusiou.win/app/truefactors/public/data.jsx',
-						'https://dev.shusiou.win/app/truefactors/public/view.jsx'	
+						'https://dev.shusiou.win/app/truefactors/public/view.jsx',
+						'https://dev.shusiou.win/app/truefactors/public/viewVideo.jsx'
 					],
 					controller : 'https://dev.shusiou.win/app/truefactors/public/controller.jsx'
 				}, 
