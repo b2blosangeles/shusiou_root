bb794c6f75d2075637bea2f4a35fbb43cd7eedfa
b2blosangeles <b2blosangeles@gmail.com>
2019-01-06 20:20:52 -0800

    Update demoAPI.js

--- api/platoplan/demoAPI.js
+++ api/platoplan/demoAPI.js
@@ -74,7 +74,7 @@ switch(req.query.code) {
 				if(!err) { cbk(tmp_plugin);
 				} else {
 					var childProcess = require('child_process');
-					var ls = childProcess.exec('ffmpeg  -i ' + plugin_video + ' -ss '+ 1 + ' -t ' + 10 + ' -c copy ' + tmp_plugin +' -y ', 		   
+					var ls = childProcess.exec('ffmpeg  -i ' + plugin_video + ' -ss '+ 1 + ' -t ' + 5 + ' -c copy ' + tmp_plugin +' -y ', 		   
 						function (error, stdout, stderr) {
 							cbk(true);
 						});
@@ -84,7 +84,7 @@ switch(req.query.code) {
 		
 		_f['batchFile'] = function(cbk) {
 			var str = "file '" + tmp_plugOrg1 + "'\n";
-			// str += "file '" + tmp_plugin + "'\n";
+			str += "file '" + tmp_plugin + "'\n";
 			str += "file '" + tmp_plugOrg2 +"'";
 			pkg.fs.writeFile(tmp_combine, str, function(err) {
 				cbk(tmp_combine);
