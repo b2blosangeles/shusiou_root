3564d7298a396783b2ec51619be5284ab669af74
b2blosangeles <b2blosangeles@gmail.com>
2019-01-06 23:08:36 -0800

    Update demoAPI.js

--- api/platoplan/demoAPI.js
+++ api/platoplan/demoAPI.js
@@ -123,9 +123,11 @@ switch(req.query.code) {
 		*/
 		_f['batchFile'] = function(cbk) {
 			var str = '';
-			str +=  "file '" + tmp_plugOrg1.replace('/tmp/', '') + "'\n";
+			
 			//str += "file '" + tmp_plugin.replace('/tmp/', '') + "'\n";
 			str += "file '" + tmp_plugOrg2.replace('/tmp/', '') +"'";
+			str +=  "file '" + tmp_plugOrg1.replace('/tmp/', '') + "'\n";
+			
 			pkg.fs.writeFile(tmp_combine, str, function(err) {
 				cbk(tmp_combine);
 			}); 
