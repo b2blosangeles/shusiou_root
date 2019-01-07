c1e7fc265a380bd1e54e5e6c261bb55b3c2b3922
b2blosangeles <b2blosangeles@gmail.com>
2019-01-06 19:58:27 -0800

    Update viewVideo.jsx

--- files/app/truefactors/inventors/viewVideo.jsx
+++ files/app/truefactors/inventors/viewVideo.jsx
@@ -47,6 +47,23 @@ var _compViewVideo = function(me, Root) {
 					</div>	
 				</div>
 			</div>)
+	};
+	this.showProgrammingPlay = function() {
+		setTimeout(function() {
+			$('#_video_section')[0].play();
+		});		
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-12 p-5 text-center alert-success">	
+					<video id="_video_section" width="260"  controls>
+						<source src={'/api/platoplan/demoAPI.api?code=playSection&fn=' + me.state.cVideo
+						 + '&s=' + s + '&l=' + l}/>
+						Your browser does not support the video tag.
+					</video>
+					</div>	
+				</div>
+			</div>)
 	};	
 	this.showSubModule = function () {
 		switch(me.state.subModule) {
@@ -57,7 +74,7 @@ var _compViewVideo = function(me, Root) {
 				return this.showSection(5, 10);
 				break;				
 			case 'ProgrammingPlay' :
-				return 'Programming Play';
+				return this.ProgrammingPlay();
 				break;				
 		}
 	}
