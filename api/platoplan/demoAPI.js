511b6bd296b2a9a035e6db0c2b0b7353596f60d6
b2blosangeles <b2blosangeles@gmail.com>
2019-01-06 23:22:34 -0800

    Update demoAPI.js

--- api/platoplan/demoAPI.js
+++ api/platoplan/demoAPI.js
@@ -125,8 +125,8 @@ switch(req.query.code) {
 			var str = '';
 			
 			//str += "file '" + tmp_plugin.replace('/tmp/', '') + "'\n";
-			str += "file '" + tmp_plugOrg2 +"'\n";
-			str +=  "file '" + tmp_plugOrg1 + "'\n";
+			str += "file '" + tmp_plugOrg2.replace('/tmp/', '') +"'\n";
+			str +=  "file '" + tmp_plugOrg1.replace('/tmp/', '') + "'\n";
 			
 			pkg.fs.writeFile(tmp_combine, str, function(err) {
 				cbk(tmp_combine);
