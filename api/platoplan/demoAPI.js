36e882295256a9bde0d5c233419fcf49770719a5
b2blosangeles <b2blosangeles@gmail.com>
2019-01-06 19:55:26 -0800

    Update demoAPI.js

--- api/platoplan/demoAPI.js
+++ api/platoplan/demoAPI.js
@@ -91,8 +91,28 @@ switch(req.query.code) {
 		CP.serial(
 			_f,
 			function(data) {
-				res.send(data);
-			}, 3000);			
+				pkg.fs.stat(tmp_output, function(err, data1) {
+					if (err) {  write404(tmp_output + ' does not exist'); }
+					else {
+						var total = data1.size;
+						var range = req.headers.range;
+						if (range) {
+						    var parts = range.replace(/bytes=/, "").split("-");
+						    var partialstart = parts[0]; var partialend;
+						      partialend =  parts[1];
+						    var start = parseInt(partialstart, 10);
+						    var end = partialend ? parseInt(partialend, 10) : total-1;
+						    var chunksize = (end-start)+1;
+						    var file = pkg.fs.createReadStream(tmp_output, {start:start, end:end});
+						    res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
+							'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+						       file.pipe(res);
+						} else {
+						    res.send('Need streaming player');
+						}
+					}
+				});
+			}, 6000);			
 		
 		break;
 	case 'cutImage':		
