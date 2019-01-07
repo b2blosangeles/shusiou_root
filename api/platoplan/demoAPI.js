069264f9e3df3c38fd76de98110268c481527ed8
b2blosangeles <b2blosangeles@gmail.com>
2019-01-06 21:09:12 -0800

    Update demoAPI.js

--- api/platoplan/demoAPI.js
+++ api/platoplan/demoAPI.js
@@ -108,7 +108,7 @@ switch(req.query.code) {
 		
 		_f['output'] = function(cbk) {
 			var childProcess = require('child_process');
-			var ls = childProcess.exec('ffmpeg -i "'+tmp_plugOrg1+'.ts|' + tmp_plugin + '.ts|' + tmp_plugOrg2 + '.ts" -c copy -bsf:a aac_adtstoasc ' + tmp_output + ' -y', 		   
+			var ls = childProcess.exec('ffmpeg -i "concat:'+tmp_plugOrg1+'.ts|' + tmp_plugin + '.ts|' + tmp_plugOrg2 + '.ts" -c copy -bsf:a aac_adtstoasc ' + tmp_output + ' -y', 		   
 				function (error, stdout, stderr) {
 					cbk('ffmpeg -i "concat:'+tmp_plugOrg1+'.ts|' + tmp_plugin + '.ts|' + tmp_plugOrg2 + '.ts" -c copy -bsf:a aac_adtstoasc ' + tmp_output + ' -y');
 				});
