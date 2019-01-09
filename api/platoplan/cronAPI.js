18d41ea93dac0b319f2e4d3438dbf19259bb2739
b2blosangeles <b2blosangeles@gmail.com>
2019-01-09 07:54:38 -0800

    Update cronAPI.js

--- api/platoplan/cronAPI.js
+++ api/platoplan/cronAPI.js
@@ -5,36 +5,41 @@ var dirn = env.root_path + '/demo_videos', dirn_formal = env.root_path + '/forma
 var _f = {};
 
 _f['fp'] = function(cbk) { 
-  var fp = new folderP();
-  fp.build(dirn_formal, function() { cbk(true);});
+	var fp = new folderP();
+	fp.build(dirn_formal, function() { 
+		cbk(true);
+	});
 };	
 f['ORG'] = function(cbk) {
-  pkg.fs.readdir(dirn, (err, files) => {
-      files.forEach(file => {
-    if (/\.mp4$/.test(file)) {
-        list.push(file);
-    }
-      });
-      cbk(list);
-  });	
+	var list = [];
+	pkg.fs.readdir(dirn, (err, files) => {
+		files.forEach(file => {
+			if (/\.mp4$/.test(file)) {
+				list.push(file);
+			}
+			
+		});
+		cbk(list);
+	});	
 }
 f['TAG'] = function(cbk) {
-  pkg.fs.readdir(dirn_formal, (err, files) => {
-      files.forEach(file => {
-    if (/\.mp4$/.test(file)) {
-        list.push(file);
-    }
-      });
-      cbk(list);
-  });	
+	var list = [];
+	pkg.fs.readdir(dirn_formal, (err, files) => {
+		files.forEach(file => {
+			if (/\.mp4$/.test(file)) {
+				list.push(file);
+			}
+		});
+		cbk(list);
+	});	
 }
 CP.serial(
-  _f,
-  function(data) {	
-    pkg.fs.stat(tmp_output, function(err, data) {
-            res.send(data);
-    });
-  }, 60000);	
+	_f,
+	function(data) {	
+		pkg.fs.stat(tmp_output, function(err, data) {
+			res.send(data);
+		});
+	}, 60000);	
 
 function write404(msg) {
 	res.writeHead(404);
