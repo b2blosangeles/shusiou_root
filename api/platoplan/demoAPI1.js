b2fd8c7afe6086adb13bd0926e78b918f83d871e
b2blosangeles <b2blosangeles@gmail.com>
2019-01-08 23:15:56 -0800

    Update demoAPI1.js

--- api/platoplan/demoAPI1.js
+++ api/platoplan/demoAPI1.js
@@ -12,7 +12,13 @@ var list = [],
 var CP = new pkg.crowdProcess();
 switch(req.query.code) {
 	case 'videoInfo':
-		res.send('videoInfo');
+		var str = 'ffmpeg -i ' + file_video;
+		var childProcess = require('child_process');
+		var ls = childProcess.exec(str, 		   
+			function (error, stdout, stderr) {
+				res.send('videoInfoA');
+			});		
+		
 		break;
 	case 'videoByScript':
 		var tmpfn = '/tmp/script_' + fn;
