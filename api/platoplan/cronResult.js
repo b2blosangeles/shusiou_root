ad75a48fad18949403f8ba77554b527a4e89ba35
b2blosangeles <b2blosangeles@gmail.com>
2019-01-09 09:05:24 -0800

    Update cronResult.js

--- api/platoplan/cronResult.js
+++ api/platoplan/cronResult.js
@@ -17,6 +17,30 @@ switch(req.query.code) {
 				res.send(dirn_formal + ' Cleaned');
 			});
 		break;
+	case 'playVideo' :
+		var file_video = (req.query.fn) ? req.query.fn : 'HEATING_JACKET.mp4',
+		pkg.fs.stat(file_video, function(err, data1) {
+			if (err) {  write404(file_video + ' does not exist'); }
+			else {
+				var total = data1.size;
+				var range = req.headers.range;
+				if (range) {
+				    var parts = range.replace(/bytes=/, "").split("-");
+				    var partialstart = parts[0]; var partialend;
+				      partialend =  parts[1];
+				    var start = parseInt(partialstart, 10);
+				    var end = partialend ? parseInt(partialend, 10) : total-1;
+				    var chunksize = (end-start)+1;
+				    var file = pkg.fs.createReadStream(file_video, {start:start, end:end});
+				    res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
+					'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+				       file.pipe(res);
+				} else {
+				    res.send('Need streaming player');
+				}
+			}
+		});		
+		
 	default :
 		var _f = {};
 
