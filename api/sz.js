0b1f49cb50b63769b86b8efd1d7966d27362a571
b2blosangeles <b2blosangeles@gmail.com>
2019-01-13 01:15:03 -0800

    Update sz.js

--- api/sz.js
+++ api/sz.js
@@ -1,8 +1,18 @@
 console.log('bu niuyy');
-req.on('data', function(data) {
-console.log('AAA');
-});
+
+var writeStream = pkg.fs.createWriteStream('/tmp/outputp.mp4');
+req.pipe(writeStream);
+/*
 req.on('end', function() {
   console.log('BBB');
 });
-res.send({success: true, code: (!req.query.code) ? '' : req.query.code});
+*/
+/*
+req.on('data', function(data) {
+console.log('AAA');
+});*/
+req.on('end', function() {
+  console.log('BBBk');
+  res.send({success: true, code: (!req.query.code) ? '' : req.query.code});
+});
+
