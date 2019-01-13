1b13b60e77c284f77b024d021d41033be230b83c
b2blosangeles <b2blosangeles@gmail.com>
2019-01-13 01:07:49 -0800

    Update sz.js

--- api/sz.js
+++ api/sz.js
@@ -1,3 +1,8 @@
-console.log('bu niu');
-console.log(req);
+console.log('bu niuyy');
+req.on('data', function(data) {
+console.log('AAA');
+});
+req.on('end', function() {
+  console.log('BBB');
+});
 res.send({success: true, code: (!req.query.code) ? '' : req.query.code});
