a481c4f491593e6b49be7e2d0882517d6fdb806e
b2blosangeles <b2blosangeles@gmail.com>
2019-01-13 09:30:08 -0800

    Update sz.js

--- api/sz.js
+++ api/sz.js
@@ -3,6 +3,18 @@ console.log('---req.files.filename--->');
 console.log(req.files);
 var writeStream = pkg.fs.createWriteStream('/var/qalet/formal_demo_videos/outputp.mov');
 req.pipe(writeStream);
+
+var Busboy = require(env.site_path + '/api/inc/busboy/node_modules/busboy');
+var busboy = new Busboy({ headers: req.headers });
+req.pipe(busboy);
+
+busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    file.on('end', function() {
+      console.log('BBBkDD');
+      res.send({success: true, code: (!req.query.code) ? '' : req.query.code});
+    });
+});
+
 /*
 req.on('end', function() {
   console.log('BBB');
@@ -12,8 +24,7 @@ req.on('end', function() {
 req.on('data', function(data) {
 console.log('AAA');
 });*/
-req.on('end', function() {
-  console.log('BBBk');
-  res.send({success: true, code: (!req.query.code) ? '' : req.query.code});
-});
+// req.on('end', function() {
+
+//});
 
