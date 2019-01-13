3159222df50172df046af674f601cb107fdae76e
b2blosangeles <b2blosangeles@gmail.com>
2019-01-13 09:54:21 -0800

    Update sz.js

--- api/sz.js
+++ api/sz.js
@@ -6,16 +6,35 @@ var Busboy = require(env.site_path + '/api/inc/busboy/node_modules/busboy');
 var busboy = new Busboy({ headers: req.headers });
 req.pipe(busboy);
 
-busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-     file.pipe(writeStream);
-     file.on('data', function(data) {
-        console.log('coming.....');
-      });
-    file.on('end', function() {
-      console.log('BBBkDD');
-      res.send({success: true, code: (!req.query.code) ? '' : req.query.code});
-    });
-});
+var CP = new pkg.crowdProcess();
+
+va['S1'] = function(cbk) {
+     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+          file.pipe(writeStream);
+          file.on('data', function(data) {});
+         file.on('end', function() {
+               cbk(true);
+         });
+     });
+};
+/*
+va['S2'] = function(cbk) {
+     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+          file.pipe(writeStream);
+          file.on('data', function(data) {});
+         file.on('end', function() {
+               cbk(true);
+         });
+     });
+};
+*/
+CP.serial(
+     _f,
+     function(data) {	
+         res.send({success: true, code: (!req.query.code) ? '' : req.query.code});
+     }, 6000);
+
+
 
 /*
 req.on('end', function() {
