30eb04469ac2785035a7afc70df47f3ad837cb52
b2blosangeles <b2blosangeles@gmail.com>
2019-01-13 09:24:29 -0800

    Update zz.js

--- api/zz.js
+++ api/zz.js
@@ -1,4 +1,4 @@
 
-var Busboy = require(env.site_path + 'api/inc/busboy/node_modules/busboy');
-res.send(env.site_path + 'api/inc/busboy/node_modules/busboy');
+var Busboy = require(env.site_path + '/api/inc/busboy/node_modules/busboy');
+res.send(env.site_path + '/api/inc/busboy/node_modules/busboy');
 // res.sendFile('/var/qalet/formal_demo_videos/ooutputp.mp4');
