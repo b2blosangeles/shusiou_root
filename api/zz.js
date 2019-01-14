4cad3b8d0309f433d8c6edf06c75f24caa4e91c8
b2blosangeles <b2blosangeles@gmail.com>
2019-01-13 19:27:37 -0800

    Delete zz.js

--- api/zz.js
+++
@@ -1,4 +0,0 @@
-
-var Busboy = require(env.site_path + '/api/inc/busboy/node_modules/busboy');
-res.send(env.site_path + '/api/inc/busboy/node_modules/busboy');
-// res.sendFile('/var/qalet/formal_demo_videos/ooutputp.mp4');
