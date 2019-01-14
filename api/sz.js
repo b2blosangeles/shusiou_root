de11536634ff1d2358377d1136fa68301564f24e
b2blosangeles <b2blosangeles@gmail.com>
2019-01-13 19:53:25 -0800

    Update sz.js

--- api/sz.js
+++ api/sz.js
@@ -1,3 +1,4 @@
+var folderP = require(env.root_path + '/package/folderP/folderP');
 var writeStream = pkg.fs.createWriteStream('/var/qalet/formal_demo_videos/outputp.mov');
 var Busboy = require(env.site_path + '/api/inc/busboy/node_modules/busboy');
 var busboy = new Busboy({ headers: req.headers });
@@ -9,6 +10,10 @@ var videoName = new Date().getTime();
 
 var CP = new pkg.crowdProcess();
 var _f = {};
+_f['fp'] = function(cbk) { 
+     var fp = new folderP();
+     fp.build(dirv + 'uploaded/', function() { cbk(true);});
+};
 _f['S1'] = function(cbk) {
      busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
           file.pipe(writeStream);
@@ -21,7 +26,7 @@ _f['S1'] = function(cbk) {
 
 _f['S2'] = function(cbk) {
      var childProcess = require('child_process');
-     var ls = childProcess.exec('ffmpeg -i ' + dirv + 'outputp.mov -vcodec copy -acodec copy ' + dirv + ' ' + videoName + '.mp4 -y',
+     var ls = childProcess.exec('ffmpeg -i ' + dirv + 'outputp.mov -vcodec copy -acodec copy ' + dirv + 'uploaded/' + videoName + '.mp4 -y',
           function (error, stdout, stderr) {
                cbk(true);
           });
