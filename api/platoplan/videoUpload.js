3062b31ae01d95603dc38f1cc136498fd6f6f270
b2blosangeles <b2blosangeles@gmail.com>
2019-02-20 22:26:31 -0800

    Update videoUpload.js

--- api/platoplan/videoUpload.js
+++ api/platoplan/videoUpload.js
@@ -1,1 +1,19 @@
-res.send({status : 'success1', tt: req.query.filename})
+// res.send({status : 'success1', tt: req.query.filename})
+var fn = (req.query.fn) ? '/tmp/' + req.query.fn : '';
+if (!fn) res.send('Missing fn');
+else {
+  pkg.fs.stat(fn, function(err, data) {
+    if (err) {  res.send(fn + ' does not exist'); }
+    else {
+        res.writeHead(200); 
+        var file = pkg.fs.createReadStream(fn);
+        file.pipe(res);
+        setTimeout(
+          function() {
+            file.destroy();
+            write404('timeout')
+          }, 30000
+        );
+      }
+  });
+}
