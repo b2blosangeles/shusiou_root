f035a1ba8440150eae7e7b15585ae1aa95928be1
b2blosangeles <b2blosangeles@gmail.com>
2019-02-21 08:47:50 -0800

    Update videoUpload.js

--- api/platoplan/videoUpload.js
+++ api/platoplan/videoUpload.js
@@ -20,6 +20,8 @@ _f['fp'] = function(cbk) {
      fp.build(dirv + 'uploaded/', function() { cbk(true);});
 };
 _f['S1'] = function(cbk) {
+     cbk(true);
+     return true
      busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
           file.pipe(writeStream);
           file.on('data', function(data) {});
