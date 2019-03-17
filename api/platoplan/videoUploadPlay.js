a50ef1e00e2df29610d46839d64d62d744e1f6a7
b2blosangeles <b2blosangeles@gmail.com>
2019-03-17 09:48:01 -0700

    Update videoUploadPlay.js

--- api/platoplan/videoUploadPlay.js
+++ api/platoplan/videoUploadPlay.js
@@ -1,1 +1,1 @@
-res.sendFile('/var/mobileCloud/250885B4-CE64-46EA-BAE3-8BCE39971E03/tmp/video_1552841139/video_0011.mp4')
+res.sendFile('/var/mobileCloud/250885B4-CE64-46EA-BAE3-8BCE39971E03/tmp/video_1552841158/video_0011.mp4')
