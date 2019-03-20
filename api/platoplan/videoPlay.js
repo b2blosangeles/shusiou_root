111ed1e54974412a5706dbbb8c03088928d1b133
b2blosangeles <b2blosangeles@gmail.com>
2019-03-20 14:05:03 -0700

    Update videoPlay.js

--- api/platoplan/videoPlay.js
+++ api/platoplan/videoPlay.js
@@ -49,13 +49,15 @@ _f['sec'] = function(cbk) {
                         str += "file '" +  ddr + items1[i] + "'\n"
                         // secList[secList.length] = ddr + items1[i]
                     }
-                    fs.writeFile("/tmp/test", "Hey there!", function(err) {
+                    /*
+                    fs.writeFile("/tmp/", "Hey there!", function(err) {
                         if(err) {
                             return console.log(err);
                         }
 
                         console.log("The file was saved!");
                     }); 
+*/
                    cbk1(true)
                 });
             }
