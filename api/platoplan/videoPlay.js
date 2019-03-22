e81c786c33da15308e466cbf09984f0bf35165ea
b2blosangeles <b2blosangeles@gmail.com>
2019-03-21 22:00:43 -0700

    Update videoPlay.js

--- api/platoplan/videoPlay.js
+++ api/platoplan/videoPlay.js
@@ -67,7 +67,33 @@ switch(req.query.code) {
 		CP.serial(
 		     _f,
 		     function(data) {
-			     res.send(data);
+			     if (CP.data.buildPath !== true) {
+				     res.send('Video ' + phoneId + ':' + vid + ' does not exist!');
+			     } else {
+				var file_video = video_dir + '/video.mp4';
+				pkg.fs.stat(file_video, function(err, data1) {
+					if (err) {  write404(file_video + ' does not exist'); }
+					else {
+						var total = data1.size;
+						var range = req.headers.range;
+						if (range) {
+						    var parts = range.replace(/bytes=/, "").split("-");
+						    var partialstart = parts[0]; var partialend;
+						      partialend =  parts[1];
+						    var start = parseInt(partialstart, 10);
+						    var end = partialend ? parseInt(partialend, 10) : total-1;
+						    var chunksize = (end-start)+1;
+						    var file = pkg.fs.createReadStream(file_video, {start:start, end:end});
+						    res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
+							'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+						       file.pipe(res);
+						} else {
+						    res.send('Need streaming player');
+						}
+					}
+				});			     
+			     } 
+			     
 		     },60000)
 		break;		
 	case 'playVideo':
