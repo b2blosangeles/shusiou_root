a1519585efebad2030d5487864cbc19e03592c27
b2blosangeles <b2blosangeles@gmail.com>
2019-05-02 05:18:41 -0700

    Update liveVideo.js

--- api/platoplan/liveVideo.js
+++ api/platoplan/liveVideo.js
@@ -121,8 +121,6 @@ CP.serial(
 				// total = 18386201
 				var range = req.headers.range;
 				if (range) {
-					res.send(range);
-					return true;
 				    var parts = range.replace(/bytes=/, "").split("-");
 					res.send(parts);
 					return true;
